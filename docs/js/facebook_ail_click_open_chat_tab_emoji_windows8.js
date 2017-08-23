1e77e12979f684b0b88bbfab9c5b7252a4e57f5c
MarkYan <mark11yan@gmail.com>
2017-08-24 07:40:00 +0800

    auto deploy on 07:40:00

--- docs/js/facebook_ail_click_open_chat_tab_emoji_windows8.js
+++ docs/js/facebook_ail_click_open_chat_tab_emoji_windows8.js
@@ -803,6 +803,7 @@ var facebook_ail_click_open_chat_tab_emoji_windows8 = {
 			[Date.UTC(2017, 7, 23), 66.670000],
 			[Date.UTC(2017, 7, 23), 66.670000],
 			[Date.UTC(2017, 7, 23), 66.670000],
+			[Date.UTC(2017, 7, 23), 66.670000],
 		]    }, {
 		name: 'chrome',
 		data: [
