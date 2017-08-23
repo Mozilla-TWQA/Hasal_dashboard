7be450ed0fea6716ced65d99a02d141bf94947fc
MarkYan <mark11yan@gmail.com>
2017-08-24 02:04:08 +0800

    auto deploy on 02:04:08

--- docs/js/amazon_ail_hover_related_product_thumbnail_windows8.js
+++ docs/js/amazon_ail_hover_related_product_thumbnail_windows8.js
@@ -1522,6 +1522,7 @@ var amazon_ail_hover_related_product_thumbnail_windows8 = {
 			[Date.UTC(2017, 7, 23), 433.330000],
 			[Date.UTC(2017, 7, 23), 433.330000],
 			[Date.UTC(2017, 7, 23), 433.330000],
+			[Date.UTC(2017, 7, 23), 444.440000],
 			[Date.UTC(2017, 7, 23), 455.560000],
 			[Date.UTC(2017, 7, 23), 455.560000],
 			[Date.UTC(2017, 7, 23), 455.560000],
--- docs/js/youtube_ail_select_search_suggestion_windows8.js
+++ docs/js/youtube_ail_select_search_suggestion_windows8.js
@@ -1458,5 +1458,6 @@ var youtube_ail_select_search_suggestion_windows8 = {
 			[Date.UTC(2017, 7, 23), 33.330000],
 			[Date.UTC(2017, 7, 23), 33.330000],
 			[Date.UTC(2017, 7, 23), 33.330000],
+			[Date.UTC(2017, 7, 23), 33.330000],
 		]    }]
 }
