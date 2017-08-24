7f2f3a778c8311caf51a5109dd8857590af3f7b7
MarkYan <mark11yan@gmail.com>
2017-08-25 04:20:55 +0800

    auto deploy on 04:20:55

--- docs/js/amazon_ail_select_search_suggestion_windows8.js
+++ docs/js/amazon_ail_select_search_suggestion_windows8.js
@@ -1529,5 +1529,6 @@ var amazon_ail_select_search_suggestion_windows8 = {
 			[Date.UTC(2017, 7, 24), 22.220000],
 			[Date.UTC(2017, 7, 24), 22.220000],
 			[Date.UTC(2017, 7, 24), 33.330000],
+			[Date.UTC(2017, 7, 24), 55.560000],
 		]    }]
 }
--- docs/js/gmail_ail_open_mail_windows10.js
+++ docs/js/gmail_ail_open_mail_windows10.js
@@ -761,6 +761,7 @@ var gmail_ail_open_mail_windows10 = {
 			[Date.UTC(2017, 7, 24), 255.560000],
 			[Date.UTC(2017, 7, 24), 266.670000],
 			[Date.UTC(2017, 7, 24), 288.890000],
+			[Date.UTC(2017, 7, 24), 333.330000],
 			[Date.UTC(2017, 7, 24), 677.780000],
 		]    }, {
 		name: 'chrome',
