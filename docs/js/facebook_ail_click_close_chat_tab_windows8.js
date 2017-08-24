f0e55b580f8aef57487666a82207a92100be96ea
MarkYan <mark11yan@gmail.com>
2017-08-24 08:41:49 +0800

    auto deploy on 08:41:49

--- docs/js/facebook_ail_click_close_chat_tab_windows8.js
+++ docs/js/facebook_ail_click_close_chat_tab_windows8.js
@@ -843,6 +843,7 @@ var facebook_ail_click_close_chat_tab_windows8 = {
 			[Date.UTC(2017, 7, 23), 44.440000],
 			[Date.UTC(2017, 7, 23), 44.440000],
 			[Date.UTC(2017, 7, 23), 44.440000],
+			[Date.UTC(2017, 7, 23), 44.440000],
 			[Date.UTC(2017, 7, 23), 55.560000],
 			[Date.UTC(2017, 7, 23), 55.560000],
 			[Date.UTC(2017, 7, 23), 55.560000],
--- docs/js/facebook_ail_type_composerbox_1_txt_windows8.js
+++ docs/js/facebook_ail_type_composerbox_1_txt_windows8.js
@@ -856,6 +856,7 @@ var facebook_ail_type_composerbox_1_txt_windows8 = {
 			[Date.UTC(2017, 7, 23), 44.440000],
 			[Date.UTC(2017, 7, 23), 44.440000],
 			[Date.UTC(2017, 7, 23), 55.560000],
+			[Date.UTC(2017, 7, 23), 55.560000],
 		]    }, {
 		name: 'chrome',
 		data: [
