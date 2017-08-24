ac20fc01576d142ae21e37625498927721a510c8
MarkYan <mark11yan@gmail.com>
2017-08-24 20:26:49 +0800

    auto deploy on 20:26:49

--- docs/js/facebook_ail_type_composerbox_1_txt_windows10.js
+++ docs/js/facebook_ail_type_composerbox_1_txt_windows10.js
@@ -485,6 +485,7 @@ var facebook_ail_type_composerbox_1_txt_windows10 = {
 			[Date.UTC(2017, 7, 4), 33.330000],
 			[Date.UTC(2017, 7, 4), 33.330000],
 			[Date.UTC(2017, 7, 4), 44.440000],
+			[Date.UTC(2017, 7, 4), 11.110000],
 			[Date.UTC(2017, 7, 4), 33.330000],
 			[Date.UTC(2017, 7, 4), 33.330000],
 			[Date.UTC(2017, 7, 4), 44.440000],
