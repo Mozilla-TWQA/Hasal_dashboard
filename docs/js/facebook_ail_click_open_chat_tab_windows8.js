efb73fbbe6cdd6ded4a414ba1003c4aaf9169ca0
MarkYan <mark11yan@gmail.com>
2017-08-24 16:49:06 +0800

    auto deploy on 16:49:06

--- docs/js/facebook_ail_click_open_chat_tab_windows8.js
+++ docs/js/facebook_ail_click_open_chat_tab_windows8.js
@@ -1048,6 +1048,7 @@ var facebook_ail_click_open_chat_tab_windows8 = {
 			[Date.UTC(2017, 6, 31), 44.440000],
 			[Date.UTC(2017, 7, 1), 100.000000],
 			[Date.UTC(2017, 7, 1), 33.330000],
+			[Date.UTC(2017, 7, 1), 33.330000],
 			[Date.UTC(2017, 7, 1), 44.440000],
 			[Date.UTC(2017, 7, 1), 44.440000],
 			[Date.UTC(2017, 7, 1), 55.560000],
