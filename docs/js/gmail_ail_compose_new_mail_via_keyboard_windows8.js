bd36321c8ea7bb6db8c3aaeea0097d5bf12f2aff
MarkYan <mark11yan@gmail.com>
2017-08-25 22:32:41 +0800

    auto deploy on 22:32:41

--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows8.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows8.js
@@ -1075,6 +1075,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows8 = {
 			[Date.UTC(2017, 6, 17), 122.220000],
 			[Date.UTC(2017, 6, 17), 122.220000],
 			[Date.UTC(2017, 6, 17), 144.440000],
+			[Date.UTC(2017, 6, 17), 144.440000],
 			[Date.UTC(2017, 6, 17), 111.110000],
 			[Date.UTC(2017, 6, 17), 122.220000],
 			[Date.UTC(2017, 6, 17), 122.220000],
