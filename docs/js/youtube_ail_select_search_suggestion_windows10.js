0d0e85e812446201d031eaaa2b02562105be23d7
MarkYan <mark11yan@gmail.com>
2017-08-27 07:24:22 +0800

    auto deploy on 07:24:22

--- docs/js/youtube_ail_select_search_suggestion_windows10.js
+++ docs/js/youtube_ail_select_search_suggestion_windows10.js
@@ -1239,5 +1239,6 @@ var youtube_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 7, 25), 5.560000],
 			[Date.UTC(2017, 7, 26), 5.560000],
 			[Date.UTC(2017, 7, 26), 5.560000],
+			[Date.UTC(2017, 7, 26), 5.560000],
 		]    }]
 }
