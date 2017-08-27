4e483b8c0db6376ce818738813ab912c3677ad94
MarkYan <mark11yan@gmail.com>
2017-08-28 05:45:44 +0800

    auto deploy on 05:45:44

--- docs/js/amazon_ail_hover_related_product_thumbnail_windows10.js
+++ docs/js/amazon_ail_hover_related_product_thumbnail_windows10.js
@@ -778,6 +778,7 @@ var amazon_ail_hover_related_product_thumbnail_windows10 = {
 			[Date.UTC(2017, 7, 27), 400.000000],
 			[Date.UTC(2017, 7, 27), 411.110000],
 			[Date.UTC(2017, 7, 27), 488.890000],
+			[Date.UTC(2017, 7, 27), 5.560000],
 		]    }, {
 		name: 'chrome',
 		data: [
--- docs/js/amazon_ail_type_in_search_field_windows10.js
+++ docs/js/amazon_ail_type_in_search_field_windows10.js
@@ -754,6 +754,7 @@ var amazon_ail_type_in_search_field_windows10 = {
 			[Date.UTC(2017, 7, 27), 11.110000],
 			[Date.UTC(2017, 7, 27), 11.110000],
 			[Date.UTC(2017, 7, 27), 22.220000],
+			[Date.UTC(2017, 7, 27), 22.220000],
 			[Date.UTC(2017, 7, 27), 5.560000],
 		]    }, {
 		name: 'chrome',
