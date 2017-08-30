e7547a143b99fdec5ace4df3428e31a6f8936be4
MarkYan <mark11yan@gmail.com>
2017-08-31 02:32:26 +0800

    auto deploy on 02:32:26

--- docs/js/gsearch_ail_select_search_suggestion_windows10.js
+++ docs/js/gsearch_ail_select_search_suggestion_windows10.js
@@ -251,5 +251,6 @@ var gsearch_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 7, 29), 5.560000],
 			[Date.UTC(2017, 7, 29), 5.560000],
 			[Date.UTC(2017, 7, 30), 5.560000],
+			[Date.UTC(2017, 7, 30), 5.560000],
 		]    }]
 }
