e73ddf54e169f59158492c1c34fe962ed21ded2a
MarkYan <mark11yan@gmail.com>
2017-08-31 03:38:08 +0800

    auto deploy on 03:38:08

--- docs/js/gsearch_ail_type_searchbox_windows10.js
+++ docs/js/gsearch_ail_type_searchbox_windows10.js
@@ -294,5 +294,7 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 7, 30), 11.110000],
 			[Date.UTC(2017, 7, 30), 5.560000],
 			[Date.UTC(2017, 7, 30), 5.560000],
+			[Date.UTC(2017, 7, 30), 5.560000],
+			[Date.UTC(2017, 7, 30), 5.560000],
 		]    }]
 }
--- docs/js/youtube_ail_select_search_suggestion_windows10.js
+++ docs/js/youtube_ail_select_search_suggestion_windows10.js
@@ -186,6 +186,7 @@ var youtube_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 7, 29), 5.560000],
 			[Date.UTC(2017, 7, 30), 11.110000],
 			[Date.UTC(2017, 7, 30), 11.110000],
+			[Date.UTC(2017, 7, 30), 11.110000],
 			[Date.UTC(2017, 7, 30), 22.220000],
 			[Date.UTC(2017, 7, 30), 33.330000],
 			[Date.UTC(2017, 7, 30), 5.560000],
