5e40235e06339a3dfdae689bef39b855e5ed804a
MarkYan <mark11yan@gmail.com>
2017-09-01 00:18:25 +0800

    auto deploy on 00:18:25

--- docs/js/facebook_ail_click_photo_viewer_right_arrow_windows8.js
+++ docs/js/facebook_ail_click_photo_viewer_right_arrow_windows8.js
@@ -317,6 +317,7 @@ var facebook_ail_click_photo_viewer_right_arrow_windows8 = {
 			[Date.UTC(2017, 7, 30), 55.560000],
 			[Date.UTC(2017, 7, 30), 55.560000],
 			[Date.UTC(2017, 7, 30), 55.560000],
+			[Date.UTC(2017, 7, 30), 55.560000],
 			[Date.UTC(2017, 7, 30), 77.780000],
 		]    }]
 }
--- docs/js/facebook_ail_type_composerbox_1_txt_windows8.js
+++ docs/js/facebook_ail_type_composerbox_1_txt_windows8.js
@@ -210,6 +210,7 @@ var facebook_ail_type_composerbox_1_txt_windows8 = {
 			[Date.UTC(2017, 7, 30), 22.220000],
 			[Date.UTC(2017, 7, 30), 33.330000],
 			[Date.UTC(2017, 7, 30), 33.330000],
+			[Date.UTC(2017, 7, 30), 33.330000],
 			[Date.UTC(2017, 7, 30), 44.440000],
 			[Date.UTC(2017, 7, 30), 55.560000],
 			[Date.UTC(2017, 7, 30), 55.560000],
