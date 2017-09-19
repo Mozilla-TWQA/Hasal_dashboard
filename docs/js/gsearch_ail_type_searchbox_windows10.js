7b0fdc1f12d0b024c4f3ff2600f0d5110aacff07
MarkYan <mark11yan@gmail.com>
2017-09-20 06:02:18 +0800

    auto deploy on 06:02:18

--- docs/js/gsearch_ail_type_searchbox_windows10.js
+++ docs/js/gsearch_ail_type_searchbox_windows10.js
@@ -293,5 +293,9 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 8, 19), 11.11],
 			[Date.UTC(2017, 8, 19), 5.56],
 			[Date.UTC(2017, 8, 19), 5.56],
+			[Date.UTC(2017, 8, 19), 5.56],
+			[Date.UTC(2017, 8, 19), 5.56],
+			[Date.UTC(2017, 8, 19), 5.56],
+			[Date.UTC(2017, 8, 19), 5.56],
 		]    }]
 }
