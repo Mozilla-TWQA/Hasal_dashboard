7b324d1574ae3da94ac18e7d516e520a149e8a9c
MarkYan <mark11yan@gmail.com>
2017-09-21 06:30:36 +0800

    auto deploy on 06:30:36

--- docs/js/gmail_ail_type_in_reply_field_windows10.js
+++ docs/js/gmail_ail_type_in_reply_field_windows10.js
@@ -252,6 +252,7 @@ var gmail_ail_type_in_reply_field_windows10 = {
 			[Date.UTC(2017, 8, 20), 11.11],
 			[Date.UTC(2017, 8, 20), 22.22],
 			[Date.UTC(2017, 8, 20), 22.22],
+			[Date.UTC(2017, 8, 20), 22.22],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -374,6 +375,7 @@ var gmail_ail_type_in_reply_field_windows10 = {
 			[Date.UTC(2017, 8, 19), 5.56],
 			[Date.UTC(2017, 8, 19), 5.56],
 			[Date.UTC(2017, 8, 19), 5.56],
+			[Date.UTC(2017, 8, 20), 22.22],
 			[Date.UTC(2017, 8, 20), 5.56],
 			[Date.UTC(2017, 8, 20), 5.56],
 			[Date.UTC(2017, 8, 20), 5.56],
