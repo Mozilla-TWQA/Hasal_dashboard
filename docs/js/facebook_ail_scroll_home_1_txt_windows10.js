123720daad87b48878fd5c440f7211306acda9ef
MarkYan <mark11yan@gmail.com>
2017-09-20 21:28:30 +0800

    auto deploy on 21:28:30

--- docs/js/facebook_ail_scroll_home_1_txt_windows10.js
+++ docs/js/facebook_ail_scroll_home_1_txt_windows10.js
@@ -209,6 +209,7 @@ var facebook_ail_scroll_home_1_txt_windows10 = {
 			[Date.UTC(2017, 8, 19), 44.44],
 			[Date.UTC(2017, 8, 19), 44.44],
 			[Date.UTC(2017, 8, 19), 44.44],
+			[Date.UTC(2017, 8, 19), 44.44],
 			[Date.UTC(2017, 8, 19), 66.67],
 		]    }, {
 		name: 'chrome',
--- docs/js/gsearch_ail_type_searchbox_windows8.js
+++ docs/js/gsearch_ail_type_searchbox_windows8.js
@@ -419,5 +419,6 @@ var gsearch_ail_type_searchbox_windows8 = {
 			[Date.UTC(2017, 8, 19), 11.11],
 			[Date.UTC(2017, 8, 19), 22.22],
 			[Date.UTC(2017, 8, 19), 22.22],
+			[Date.UTC(2017, 8, 19), 22.22],
 		]    }]
 }
