d79e523c6312e26f5cc883411555f708110a5af9
MarkYan <mark11yan@gmail.com>
2017-09-20 17:29:02 +0800

    auto deploy on 17:29:02

--- docs/js/gsearch_ail_select_search_suggestion_windows10.js
+++ docs/js/gsearch_ail_select_search_suggestion_windows10.js
@@ -248,6 +248,7 @@ var gsearch_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 8, 19), 33.33],
 			[Date.UTC(2017, 8, 19), 33.33],
 			[Date.UTC(2017, 8, 19), 5.56],
+			[Date.UTC(2017, 8, 19), 5.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -403,6 +404,7 @@ var gsearch_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 8, 18), 5.56],
 			[Date.UTC(2017, 8, 19), 5.56],
 			[Date.UTC(2017, 8, 19), 22.22],
+			[Date.UTC(2017, 8, 19), 5.56],
 			[Date.UTC(2017, 8, 19), 5.56],
 			[Date.UTC(2017, 8, 19), 5.56],
 			[Date.UTC(2017, 8, 19), 5.56],
