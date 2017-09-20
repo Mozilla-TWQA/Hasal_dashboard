4373fba78c6228ec7e9174b0706783d791a481a2
MarkYan <mark11yan@gmail.com>
2017-09-20 23:08:06 +0800

    auto deploy on 23:08:06

--- docs/js/amazon_ail_hover_related_product_thumbnail_windows8.js
+++ docs/js/amazon_ail_hover_related_product_thumbnail_windows8.js
@@ -185,6 +185,7 @@ var amazon_ail_hover_related_product_thumbnail_windows8 = {
 			[Date.UTC(2017, 8, 17), 433.33],
 			[Date.UTC(2017, 8, 17), 433.33],
 			[Date.UTC(2017, 8, 17), 433.33],
+			[Date.UTC(2017, 8, 17), 433.33],
 			[Date.UTC(2017, 8, 17), 444.44],
 			[Date.UTC(2017, 8, 17), 444.44],
 			[Date.UTC(2017, 8, 17), 444.44],
@@ -229,7 +230,9 @@ var amazon_ail_hover_related_product_thumbnail_windows8 = {
 			[Date.UTC(2017, 8, 19), 444.44],
 			[Date.UTC(2017, 8, 19), 444.44],
 			[Date.UTC(2017, 8, 19), 477.78],
+			[Date.UTC(2017, 8, 19), 411.11],
 			[Date.UTC(2017, 8, 19), 422.22],
+			[Date.UTC(2017, 8, 19), 488.89],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -403,6 +406,7 @@ var amazon_ail_hover_related_product_thumbnail_windows8 = {
 			[Date.UTC(2017, 8, 19), 522.22],
 			[Date.UTC(2017, 8, 19), 533.33],
 			[Date.UTC(2017, 8, 19), 566.67],
+			[Date.UTC(2017, 8, 19), 533.33],
 			[Date.UTC(2017, 8, 19), 533.33],
 			[Date.UTC(2017, 8, 19), 544.44],
 		]    }]
--- docs/js/facebook_ail_click_open_chat_tab_emoji_windows10.js
+++ docs/js/facebook_ail_click_open_chat_tab_emoji_windows10.js
@@ -341,6 +341,7 @@ var facebook_ail_click_open_chat_tab_emoji_windows10 = {
 			[Date.UTC(2017, 8, 19), 11.11],
 			[Date.UTC(2017, 8, 19), 11.11],
 			[Date.UTC(2017, 8, 19), 11.11],
+			[Date.UTC(2017, 8, 19), 11.11],
 			[Date.UTC(2017, 8, 19), 22.22],
 			[Date.UTC(2017, 8, 19), 22.22],
 			[Date.UTC(2017, 8, 19), 33.33],
