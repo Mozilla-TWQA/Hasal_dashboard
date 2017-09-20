44f752a3ea00f750c63d36aede38e72056ca32eb
MarkYan <mark11yan@gmail.com>
2017-09-20 21:55:12 +0800

    auto deploy on 21:55:12

--- docs/js/facebook_ail_click_photo_viewer_right_arrow_windows8.js
+++ docs/js/facebook_ail_click_photo_viewer_right_arrow_windows8.js
@@ -229,6 +229,7 @@ var facebook_ail_click_photo_viewer_right_arrow_windows8 = {
 			[Date.UTC(2017, 8, 19), 77.78],
 			[Date.UTC(2017, 8, 19), 77.78],
 			[Date.UTC(2017, 8, 19), 77.78],
+			[Date.UTC(2017, 8, 19), 77.78],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -392,6 +393,7 @@ var facebook_ail_click_photo_viewer_right_arrow_windows8 = {
 			[Date.UTC(2017, 8, 19), 88.89],
 			[Date.UTC(2017, 8, 19), 55.56],
 			[Date.UTC(2017, 8, 19), 66.67],
+			[Date.UTC(2017, 8, 19), 88.89],
 			[Date.UTC(2017, 8, 19), 88.89],
 			[Date.UTC(2017, 8, 19), 88.89],
 			[Date.UTC(2017, 8, 19), 88.89],
--- docs/js/facebook_ail_type_composerbox_1_txt_windows8.js
+++ docs/js/facebook_ail_type_composerbox_1_txt_windows8.js
@@ -217,6 +217,7 @@ var facebook_ail_type_composerbox_1_txt_windows8 = {
 			[Date.UTC(2017, 8, 19), 55.56],
 			[Date.UTC(2017, 8, 19), 66.67],
 			[Date.UTC(2017, 8, 19), 66.67],
+			[Date.UTC(2017, 8, 19), 44.44],
 			[Date.UTC(2017, 8, 19), 66.67],
 		]    }, {
 		name: 'chrome',
@@ -388,6 +389,7 @@ var facebook_ail_type_composerbox_1_txt_windows8 = {
 			[Date.UTC(2017, 8, 19), 33.33],
 			[Date.UTC(2017, 8, 19), 33.33],
 			[Date.UTC(2017, 8, 19), 44.44],
+			[Date.UTC(2017, 8, 19), 33.33],
 			[Date.UTC(2017, 8, 19), 33.33],
 		]    }]
 }
