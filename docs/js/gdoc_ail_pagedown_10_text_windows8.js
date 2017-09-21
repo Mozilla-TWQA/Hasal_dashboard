f448d3f4e37136903b6c835729657f4ddb8bd9d9
MarkYan <mark11yan@gmail.com>
2017-09-21 14:32:24 +0800

    auto deploy on 14:32:24

--- docs/js/gdoc_ail_pagedown_10_text_windows8.js
+++ docs/js/gdoc_ail_pagedown_10_text_windows8.js
@@ -286,6 +286,7 @@ var gdoc_ail_pagedown_10_text_windows8 = {
 			[Date.UTC(2017, 8, 20), 55.56],
 			[Date.UTC(2017, 8, 20), 55.56],
 			[Date.UTC(2017, 8, 20), 55.56],
+			[Date.UTC(2017, 8, 20), 55.56],
 			[Date.UTC(2017, 8, 20), 66.67],
 		]    }, {
 		name: 'chrome',
