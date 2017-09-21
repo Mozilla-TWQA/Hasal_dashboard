45a3511797f95cbac27af9033a0dd9c4d932e5c9
MarkYan <mark11yan@gmail.com>
2017-09-22 06:31:21 +0800

    auto deploy on 06:31:21

--- docs/js/facebook_ail_click_open_chat_tab_emoji_windows8.js
+++ docs/js/facebook_ail_click_open_chat_tab_emoji_windows8.js
@@ -279,6 +279,7 @@ var facebook_ail_click_open_chat_tab_emoji_windows8 = {
 			[Date.UTC(2017, 8, 17), 88.89],
 			[Date.UTC(2017, 8, 17), 88.89],
 			[Date.UTC(2017, 8, 17), 88.89],
+			[Date.UTC(2017, 8, 17), 88.89],
 			[Date.UTC(2017, 8, 18), 100.0],
 			[Date.UTC(2017, 8, 18), 66.67],
 			[Date.UTC(2017, 8, 18), 77.78],
--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows8.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows8.js
@@ -248,6 +248,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows8 = {
 			[Date.UTC(2017, 8, 17), 222.22],
 			[Date.UTC(2017, 8, 17), 222.22],
 			[Date.UTC(2017, 8, 17), 222.22],
+			[Date.UTC(2017, 8, 17), 222.22],
 			[Date.UTC(2017, 8, 17), 233.33],
 			[Date.UTC(2017, 8, 17), 233.33],
 			[Date.UTC(2017, 8, 17), 233.33],
