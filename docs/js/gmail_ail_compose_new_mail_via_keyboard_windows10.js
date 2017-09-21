0398782828ca80d5ebecee5beb373e72c7fac3c6
MarkYan <mark11yan@gmail.com>
2017-09-21 13:06:36 +0800

    auto deploy on 13:06:36

--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
@@ -247,6 +247,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 8, 20), 244.44],
 			[Date.UTC(2017, 8, 20), 244.44],
 			[Date.UTC(2017, 8, 20), 200.0],
+			[Date.UTC(2017, 8, 20), 200.0],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -393,6 +394,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 8, 20), 111.11],
 			[Date.UTC(2017, 8, 20), 133.33],
 			[Date.UTC(2017, 8, 20), 133.33],
+			[Date.UTC(2017, 8, 20), 122.22],
 			[Date.UTC(2017, 8, 20), 122.22],
 		]    }]
 }
--- docs/js/youtube_ail_select_search_suggestion_windows8.js
+++ docs/js/youtube_ail_select_search_suggestion_windows8.js
@@ -222,6 +222,7 @@ var youtube_ail_select_search_suggestion_windows8 = {
 			[Date.UTC(2017, 8, 17), 33.33],
 			[Date.UTC(2017, 8, 17), 33.33],
 			[Date.UTC(2017, 8, 17), 33.33],
+			[Date.UTC(2017, 8, 17), 33.33],
 			[Date.UTC(2017, 8, 17), 44.44],
 			[Date.UTC(2017, 8, 17), 44.44],
 			[Date.UTC(2017, 8, 17), 44.44],
@@ -497,6 +498,7 @@ var youtube_ail_select_search_suggestion_windows8 = {
 			[Date.UTC(2017, 8, 20), 66.67],
 			[Date.UTC(2017, 8, 20), 22.22],
 			[Date.UTC(2017, 8, 20), 22.22],
+			[Date.UTC(2017, 8, 20), 33.33],
 			[Date.UTC(2017, 8, 20), 33.33],
 		]    }]
 }
