756dcb5493b4d082164932eecd5da777fbd65db1
MarkYan <mark11yan@gmail.com>
2017-09-21 22:12:09 +0800

    auto deploy on 22:12:09

--- docs/js/gmail_ail_open_mail_windows8.js
+++ docs/js/gmail_ail_open_mail_windows8.js
@@ -299,6 +299,7 @@ var gmail_ail_open_mail_windows8 = {
 			[Date.UTC(2017, 8, 20), 244.44],
 			[Date.UTC(2017, 8, 20), 244.44],
 			[Date.UTC(2017, 8, 20), 255.56],
+			[Date.UTC(2017, 8, 20), 255.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -512,6 +513,7 @@ var gmail_ail_open_mail_windows8 = {
 			[Date.UTC(2017, 8, 20), 222.22],
 			[Date.UTC(2017, 8, 20), 233.33],
 			[Date.UTC(2017, 8, 20), 255.56],
+			[Date.UTC(2017, 8, 20), 177.78],
 			[Date.UTC(2017, 8, 20), 177.78],
 			[Date.UTC(2017, 8, 20), 177.78],
 			[Date.UTC(2017, 8, 20), 188.89],
