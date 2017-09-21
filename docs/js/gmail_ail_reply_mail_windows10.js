d1213177415a581bd53c3a39f21b22917d119aa6
MarkYan <mark11yan@gmail.com>
2017-09-21 09:28:48 +0800

    auto deploy on 09:28:48

--- docs/js/gmail_ail_reply_mail_windows10.js
+++ docs/js/gmail_ail_reply_mail_windows10.js
@@ -234,6 +234,7 @@ var gmail_ail_reply_mail_windows10 = {
 			[Date.UTC(2017, 8, 19), 300.0],
 			[Date.UTC(2017, 8, 19), 311.11],
 			[Date.UTC(2017, 8, 20), 244.44],
+			[Date.UTC(2017, 8, 20), 266.67],
 		]    }, {
 		name: 'chrome',
 		data: [
