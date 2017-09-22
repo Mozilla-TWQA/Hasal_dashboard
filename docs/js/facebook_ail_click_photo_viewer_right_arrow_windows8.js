7a6e7a5dbcc3224def1c55a41f1e77435e1df356
MarkYan <mark11yan@gmail.com>
2017-09-22 10:46:13 +0800

    auto deploy on 10:46:13

--- docs/js/facebook_ail_click_photo_viewer_right_arrow_windows8.js
+++ docs/js/facebook_ail_click_photo_viewer_right_arrow_windows8.js
@@ -315,6 +315,7 @@ var facebook_ail_click_photo_viewer_right_arrow_windows8 = {
 			[Date.UTC(2017, 8, 21), 100.0],
 			[Date.UTC(2017, 8, 21), 66.67],
 			[Date.UTC(2017, 8, 21), 77.78],
+			[Date.UTC(2017, 8, 21), 77.78],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -531,6 +532,7 @@ var facebook_ail_click_photo_viewer_right_arrow_windows8 = {
 			[Date.UTC(2017, 8, 20), 88.89],
 			[Date.UTC(2017, 8, 20), 88.89],
 			[Date.UTC(2017, 8, 21), 111.11],
+			[Date.UTC(2017, 8, 21), 33.33],
 			[Date.UTC(2017, 8, 21), 55.56],
 			[Date.UTC(2017, 8, 21), 55.56],
 		]    }]
