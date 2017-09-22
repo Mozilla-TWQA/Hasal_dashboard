4fe09d8e50788ec79bbca49261020b1da8aca846
MarkYan <mark11yan@gmail.com>
2017-09-22 17:55:06 +0800

    auto deploy on 17:55:06

--- docs/js/facebook_ail_type_comment_1_txt_windows8.js
+++ docs/js/facebook_ail_type_comment_1_txt_windows8.js
@@ -505,6 +505,7 @@ var facebook_ail_type_comment_1_txt_windows8 = {
 			[Date.UTC(2017, 8, 19), 33.33],
 			[Date.UTC(2017, 8, 19), 33.33],
 			[Date.UTC(2017, 8, 19), 44.44],
+			[Date.UTC(2017, 8, 19), 44.44],
 			[Date.UTC(2017, 8, 19), 22.22],
 			[Date.UTC(2017, 8, 19), 33.33],
 			[Date.UTC(2017, 8, 20), 22.22],
