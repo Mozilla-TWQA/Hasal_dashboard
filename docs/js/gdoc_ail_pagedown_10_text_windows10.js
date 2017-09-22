4157c898d3c51df9c25c94c1aac93c9f623ee32c
MarkYan <mark11yan@gmail.com>
2017-09-23 04:33:46 +0800

    auto deploy on 04:33:46

--- docs/js/gdoc_ail_pagedown_10_text_windows10.js
+++ docs/js/gdoc_ail_pagedown_10_text_windows10.js
@@ -445,5 +445,6 @@ var gdoc_ail_pagedown_10_text_windows10 = {
 			[Date.UTC(2017, 8, 22), 22.22],
 			[Date.UTC(2017, 8, 22), 22.22],
 			[Date.UTC(2017, 8, 22), 5.56],
+			[Date.UTC(2017, 8, 22), 5.56],
 		]    }]
 }
--- docs/js/gsearch_ail_select_image_cat_windows10.js
+++ docs/js/gsearch_ail_select_image_cat_windows10.js
@@ -261,6 +261,8 @@ var gsearch_ail_select_image_cat_windows10 = {
 			[Date.UTC(2017, 8, 21), 88.89],
 			[Date.UTC(2017, 8, 21), 88.89],
 			[Date.UTC(2017, 8, 22), 100.0],
+			[Date.UTC(2017, 8, 22), 100.0],
+			[Date.UTC(2017, 8, 22), 100.0],
 			[Date.UTC(2017, 8, 22), 111.11],
 			[Date.UTC(2017, 8, 22), 222.22],
 			[Date.UTC(2017, 8, 22), 88.89],
@@ -410,6 +412,7 @@ var gsearch_ail_select_image_cat_windows10 = {
 			[Date.UTC(2017, 8, 21), 66.67],
 			[Date.UTC(2017, 8, 21), 77.78],
 			[Date.UTC(2017, 8, 21), 88.89],
+			[Date.UTC(2017, 8, 22), 166.67],
 			[Date.UTC(2017, 8, 22), 88.89],
 			[Date.UTC(2017, 8, 22), 88.89],
 		]    }]
