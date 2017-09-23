01856f32e9c29afa5df8bb7b1f215f1742851442
MarkYan <mark11yan@gmail.com>
2017-09-23 15:25:56 +0800

    auto deploy on 15:25:56

--- docs/js/facebook_ail_type_composerbox_1_txt_windows8.js
+++ docs/js/facebook_ail_type_composerbox_1_txt_windows8.js
@@ -617,5 +617,6 @@ var facebook_ail_type_composerbox_1_txt_windows8 = {
 			[Date.UTC(2017, 8, 22), 33.33],
 			[Date.UTC(2017, 8, 22), 33.33],
 			[Date.UTC(2017, 8, 22), 44.44],
+			[Date.UTC(2017, 8, 22), 33.33],
 		]    }]
 }
--- docs/js/facebook_ail_type_message_1_txt_windows8.js
+++ docs/js/facebook_ail_type_message_1_txt_windows8.js
@@ -357,6 +357,7 @@ var facebook_ail_type_message_1_txt_windows8 = {
 			[Date.UTC(2017, 8, 22), 66.67],
 			[Date.UTC(2017, 8, 22), 22.22],
 			[Date.UTC(2017, 8, 22), 33.33],
+			[Date.UTC(2017, 8, 22), 33.33],
 			[Date.UTC(2017, 8, 22), 44.44],
 			[Date.UTC(2017, 8, 22), 44.44],
 		]    }, {
@@ -560,6 +561,7 @@ var facebook_ail_type_message_1_txt_windows8 = {
 			[Date.UTC(2017, 8, 22), 33.33],
 			[Date.UTC(2017, 8, 22), 33.33],
 			[Date.UTC(2017, 8, 22), 33.33],
+			[Date.UTC(2017, 8, 22), 22.22],
 			[Date.UTC(2017, 8, 22), 22.22],
 			[Date.UTC(2017, 8, 22), 22.22],
 			[Date.UTC(2017, 8, 22), 33.33],
