25cb22a0b7893a0d482952a5ddb36feee4e76075
MarkYan <mark11yan@gmail.com>
2017-09-23 15:19:16 +0800

    auto deploy on 15:19:16

--- docs/js/facebook_ail_type_message_1_txt_windows8.js
+++ docs/js/facebook_ail_type_message_1_txt_windows8.js
@@ -358,6 +358,7 @@ var facebook_ail_type_message_1_txt_windows8 = {
 			[Date.UTC(2017, 8, 22), 22.22],
 			[Date.UTC(2017, 8, 22), 33.33],
 			[Date.UTC(2017, 8, 22), 44.44],
+			[Date.UTC(2017, 8, 22), 44.44],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -559,6 +560,7 @@ var facebook_ail_type_message_1_txt_windows8 = {
 			[Date.UTC(2017, 8, 22), 33.33],
 			[Date.UTC(2017, 8, 22), 33.33],
 			[Date.UTC(2017, 8, 22), 33.33],
+			[Date.UTC(2017, 8, 22), 22.22],
 			[Date.UTC(2017, 8, 22), 22.22],
 			[Date.UTC(2017, 8, 22), 33.33],
 			[Date.UTC(2017, 8, 22), 33.33],
--- docs/js/gsearch_ail_select_search_suggestion_windows8.js
+++ docs/js/gsearch_ail_select_search_suggestion_windows8.js
@@ -373,6 +373,9 @@ var gsearch_ail_select_search_suggestion_windows8 = {
 			[Date.UTC(2017, 8, 22), 33.33],
 			[Date.UTC(2017, 8, 22), 33.33],
 			[Date.UTC(2017, 8, 22), 33.33],
+			[Date.UTC(2017, 8, 22), 33.33],
+			[Date.UTC(2017, 8, 22), 44.44],
+			[Date.UTC(2017, 8, 22), 44.44],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -627,5 +630,8 @@ var gsearch_ail_select_search_suggestion_windows8 = {
 			[Date.UTC(2017, 8, 22), 11.11],
 			[Date.UTC(2017, 8, 22), 11.11],
 			[Date.UTC(2017, 8, 22), 22.22],
+			[Date.UTC(2017, 8, 22), 22.22],
+			[Date.UTC(2017, 8, 22), 22.22],
+			[Date.UTC(2017, 8, 22), 33.33],
 		]    }]
 }
