83e068e174a395e3be8ebcf030b320756ba29c03
MarkYan <mark11yan@gmail.com>
2017-09-26 04:03:03 +0800

    auto deploy on 04:03:03

--- docs/js/facebook_ail_scroll_home_1_txt_windows10.js
+++ docs/js/facebook_ail_scroll_home_1_txt_windows10.js
@@ -263,6 +263,7 @@ var facebook_ail_scroll_home_1_txt_windows10 = {
 			[Date.UTC(2017, 8, 25), 44.44],
 			[Date.UTC(2017, 8, 25), 55.56],
 			[Date.UTC(2017, 8, 25), 55.56],
+			[Date.UTC(2017, 8, 25), 55.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -432,6 +433,7 @@ var facebook_ail_scroll_home_1_txt_windows10 = {
 			[Date.UTC(2017, 8, 24), 33.33],
 			[Date.UTC(2017, 8, 24), 33.33],
 			[Date.UTC(2017, 8, 25), 11.11],
+			[Date.UTC(2017, 8, 25), 22.22],
 			[Date.UTC(2017, 8, 25), 22.22],
 			[Date.UTC(2017, 8, 25), 22.22],
 			[Date.UTC(2017, 8, 25), 5.56],
--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
@@ -278,6 +278,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 8, 25), 233.33],
 			[Date.UTC(2017, 8, 25), 233.33],
 			[Date.UTC(2017, 8, 25), 233.33],
+			[Date.UTC(2017, 8, 25), 255.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -455,6 +456,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 8, 25), 100.0],
 			[Date.UTC(2017, 8, 25), 111.11],
 			[Date.UTC(2017, 8, 25), 111.11],
+			[Date.UTC(2017, 8, 25), 122.22],
 			[Date.UTC(2017, 8, 25), 122.22],
 			[Date.UTC(2017, 8, 25), 155.56],
 		]    }]
