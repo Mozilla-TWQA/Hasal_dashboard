c42885e1438ee27a8cba835ec4813f707fc80572
MarkYan <mark11yan@gmail.com>
2017-09-25 14:01:30 +0800

    auto deploy on 14:01:30

--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows8.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows8.js
@@ -379,6 +379,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows8 = {
 			[Date.UTC(2017, 8, 24), 222.22],
 			[Date.UTC(2017, 8, 24), 222.22],
 			[Date.UTC(2017, 8, 24), 222.22],
+			[Date.UTC(2017, 8, 24), 233.33],
 		]    }, {
 		name: 'chrome',
 		data: [
--- docs/js/gsearch_ail_select_image_cat_windows10.js
+++ docs/js/gsearch_ail_select_image_cat_windows10.js
@@ -284,6 +284,7 @@ var gsearch_ail_select_image_cat_windows10 = {
 			[Date.UTC(2017, 8, 24), 100.0],
 			[Date.UTC(2017, 8, 24), 111.11],
 			[Date.UTC(2017, 8, 24), 77.78],
+			[Date.UTC(2017, 8, 24), 77.78],
 			[Date.UTC(2017, 8, 24), 88.89],
 			[Date.UTC(2017, 8, 24), 88.89],
 		]    }, {
@@ -450,6 +451,7 @@ var gsearch_ail_select_image_cat_windows10 = {
 			[Date.UTC(2017, 8, 24), 88.89],
 			[Date.UTC(2017, 8, 24), 144.44],
 			[Date.UTC(2017, 8, 24), 155.56],
+			[Date.UTC(2017, 8, 24), 166.67],
 			[Date.UTC(2017, 8, 24), 166.67],
 			[Date.UTC(2017, 8, 24), 166.67],
 			[Date.UTC(2017, 8, 24), 66.67],
