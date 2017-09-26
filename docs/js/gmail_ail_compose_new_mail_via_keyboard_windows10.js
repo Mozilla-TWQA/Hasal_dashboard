fc92fd5a48f156fb2e5d81b2aea5540113922dd5
MarkYan <mark11yan@gmail.com>
2017-09-27 02:50:18 +0800

    auto deploy on 02:50:18

--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
@@ -300,6 +300,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 8, 26), 211.11],
 			[Date.UTC(2017, 8, 26), 211.11],
 			[Date.UTC(2017, 8, 26), 222.22],
+			[Date.UTC(2017, 8, 26), 233.33],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -487,6 +488,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 8, 26), 100.0],
 			[Date.UTC(2017, 8, 26), 111.11],
 			[Date.UTC(2017, 8, 26), 111.11],
+			[Date.UTC(2017, 8, 26), 122.22],
 			[Date.UTC(2017, 8, 26), 122.22],
 			[Date.UTC(2017, 8, 26), 133.33],
 			[Date.UTC(2017, 8, 26), 133.33],
