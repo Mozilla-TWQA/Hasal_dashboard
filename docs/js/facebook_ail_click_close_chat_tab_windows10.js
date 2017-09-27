e1237f4b7cc2d2879a78af7551353a8502ed6d7c
MarkYan <mark11yan@gmail.com>
2017-09-28 04:26:47 +0800

    auto deploy on 04:26:47

--- docs/js/facebook_ail_click_close_chat_tab_windows10.js
+++ docs/js/facebook_ail_click_close_chat_tab_windows10.js
@@ -313,6 +313,7 @@ var facebook_ail_click_close_chat_tab_windows10 = {
 			[Date.UTC(2017, 8, 26), 55.56],
 			[Date.UTC(2017, 8, 26), 66.67],
 			[Date.UTC(2017, 8, 26), 66.67],
+			[Date.UTC(2017, 8, 27), 100.0],
 			[Date.UTC(2017, 8, 27), 22.22],
 			[Date.UTC(2017, 8, 27), 44.44],
 			[Date.UTC(2017, 8, 27), 55.56],
--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
@@ -310,7 +310,9 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 8, 26), 255.56],
 			[Date.UTC(2017, 8, 27), 188.89],
 			[Date.UTC(2017, 8, 27), 188.89],
+			[Date.UTC(2017, 8, 27), 200.0],
 			[Date.UTC(2017, 8, 27), 222.22],
+			[Date.UTC(2017, 8, 27), 244.44],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -513,6 +515,8 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 8, 27), 122.22],
 			[Date.UTC(2017, 8, 27), 122.22],
 			[Date.UTC(2017, 8, 27), 122.22],
+			[Date.UTC(2017, 8, 27), 122.22],
+			[Date.UTC(2017, 8, 27), 133.33],
 			[Date.UTC(2017, 8, 27), 133.33],
 		]    }]
 }
