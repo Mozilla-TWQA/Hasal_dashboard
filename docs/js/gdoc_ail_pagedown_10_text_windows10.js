143977f8c95922cb289446414ed5df8dbfa1bbc0
MarkYan <mark11yan@gmail.com>
2017-09-28 05:18:38 +0800

    auto deploy on 05:18:38

--- docs/js/gdoc_ail_pagedown_10_text_windows10.js
+++ docs/js/gdoc_ail_pagedown_10_text_windows10.js
@@ -524,6 +524,7 @@ var gdoc_ail_pagedown_10_text_windows10 = {
 			[Date.UTC(2017, 8, 27), 11.11],
 			[Date.UTC(2017, 8, 27), 22.22],
 			[Date.UTC(2017, 8, 27), 22.22],
+			[Date.UTC(2017, 8, 27), 22.22],
 			[Date.UTC(2017, 8, 27), 233.33],
 			[Date.UTC(2017, 8, 27), 33.33],
 			[Date.UTC(2017, 8, 27), 33.33],
--- docs/js/gsearch_ail_type_searchbox_windows10.js
+++ docs/js/gsearch_ail_type_searchbox_windows10.js
@@ -379,6 +379,7 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 8, 26), 5.56],
 			[Date.UTC(2017, 8, 26), 5.56],
 			[Date.UTC(2017, 8, 26), 5.56],
+			[Date.UTC(2017, 8, 27), 11.11],
 			[Date.UTC(2017, 8, 27), 5.56],
 			[Date.UTC(2017, 8, 27), 5.56],
 			[Date.UTC(2017, 8, 27), 5.56],
