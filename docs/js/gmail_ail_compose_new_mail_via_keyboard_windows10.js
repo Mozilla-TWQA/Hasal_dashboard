8b71342600585503f5dd6d907381ed32bb64c2bb
MarkYan <mark11yan@gmail.com>
2017-09-29 01:42:11 +0800

    auto deploy on 01:42:11

--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
@@ -313,6 +313,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 8, 28), 177.78],
 			[Date.UTC(2017, 8, 28), 200.0],
 			[Date.UTC(2017, 8, 28), 200.0],
+			[Date.UTC(2017, 8, 28), 200.0],
 			[Date.UTC(2017, 8, 28), 211.11],
 			[Date.UTC(2017, 8, 28), 211.11],
 			[Date.UTC(2017, 8, 28), 222.22],
@@ -530,6 +531,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 8, 28), 122.22],
 			[Date.UTC(2017, 8, 28), 122.22],
 			[Date.UTC(2017, 8, 28), 122.22],
+			[Date.UTC(2017, 8, 28), 133.33],
 			[Date.UTC(2017, 8, 28), 88.89],
 		]    }]
 }
--- docs/js/youtube_ail_type_in_search_field_windows10.js
+++ docs/js/youtube_ail_type_in_search_field_windows10.js
@@ -311,6 +311,7 @@ var youtube_ail_type_in_search_field_windows10 = {
 			[Date.UTC(2017, 8, 27), 22.22],
 			[Date.UTC(2017, 8, 27), 22.22],
 			[Date.UTC(2017, 8, 27), 66.67],
+			[Date.UTC(2017, 8, 28), 11.11],
 			[Date.UTC(2017, 8, 28), 22.22],
 			[Date.UTC(2017, 8, 28), 22.22],
 			[Date.UTC(2017, 8, 28), 22.22],
