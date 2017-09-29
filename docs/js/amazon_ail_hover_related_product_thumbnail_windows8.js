6e9554485dcae0bbae9a77ce355dccd72af72e9d
MarkYan <mark11yan@gmail.com>
2017-09-30 01:49:55 +0800

    auto deploy on 01:49:55

--- docs/js/amazon_ail_hover_related_product_thumbnail_windows8.js
+++ docs/js/amazon_ail_hover_related_product_thumbnail_windows8.js
@@ -396,6 +396,7 @@ var amazon_ail_hover_related_product_thumbnail_windows8 = {
 			[Date.UTC(2017, 8, 29), 444.44],
 			[Date.UTC(2017, 8, 29), 444.44],
 			[Date.UTC(2017, 8, 29), 444.44],
+			[Date.UTC(2017, 8, 29), 444.44],
 			[Date.UTC(2017, 8, 29), 455.56],
 			[Date.UTC(2017, 8, 29), 477.78],
 		]    }, {
@@ -663,6 +664,7 @@ var amazon_ail_hover_related_product_thumbnail_windows8 = {
 			[Date.UTC(2017, 8, 29), 444.44],
 			[Date.UTC(2017, 8, 29), 477.78],
 			[Date.UTC(2017, 8, 29), 477.78],
+			[Date.UTC(2017, 8, 29), 488.89],
 			[Date.UTC(2017, 8, 29), 488.89],
 			[Date.UTC(2017, 8, 29), 500.0],
 			[Date.UTC(2017, 8, 29), 533.33],
--- docs/js/facebook_ail_type_comment_1_txt_windows10.js
+++ docs/js/facebook_ail_type_comment_1_txt_windows10.js
@@ -259,6 +259,7 @@ var facebook_ail_type_comment_1_txt_windows10 = {
 			[Date.UTC(2017, 8, 28), 33.33],
 			[Date.UTC(2017, 8, 28), 44.44],
 			[Date.UTC(2017, 8, 29), 33.33],
+			[Date.UTC(2017, 8, 29), 33.33],
 			[Date.UTC(2017, 8, 29), 44.44],
 			[Date.UTC(2017, 8, 29), 44.44],
 			[Date.UTC(2017, 8, 29), 44.44],
