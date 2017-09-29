cac8c1942afaa82c0b6662a8bea54cf39db4ed8e
MarkYan <mark11yan@gmail.com>
2017-09-30 02:16:04 +0800

    auto deploy on 02:16:04

--- docs/js/gdoc_ail_pagedown_10_text_windows10.js
+++ docs/js/gdoc_ail_pagedown_10_text_windows10.js
@@ -327,6 +327,8 @@ var gdoc_ail_pagedown_10_text_windows10 = {
 			[Date.UTC(2017, 8, 29), 44.44],
 			[Date.UTC(2017, 8, 29), 55.56],
 			[Date.UTC(2017, 8, 29), 55.56],
+			[Date.UTC(2017, 8, 29), 55.56],
+			[Date.UTC(2017, 8, 29), 55.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -550,6 +552,7 @@ var gdoc_ail_pagedown_10_text_windows10 = {
 			[Date.UTC(2017, 8, 28), 5.56],
 			[Date.UTC(2017, 8, 29), 133.33],
 			[Date.UTC(2017, 8, 29), 22.22],
+			[Date.UTC(2017, 8, 29), 33.33],
 			[Date.UTC(2017, 8, 29), 5.56],
 		]    }]
 }
--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
@@ -540,6 +540,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 8, 29), 100.0],
 			[Date.UTC(2017, 8, 29), 100.0],
 			[Date.UTC(2017, 8, 29), 111.11],
+			[Date.UTC(2017, 8, 29), 111.11],
 			[Date.UTC(2017, 8, 29), 122.22],
 			[Date.UTC(2017, 8, 29), 88.89],
 		]    }]
