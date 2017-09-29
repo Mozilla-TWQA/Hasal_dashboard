32bb160d2dd8ef847067f0311722aac54f213d66
MarkYan <mark11yan@gmail.com>
2017-09-29 14:35:33 +0800

    auto deploy on 14:35:33

--- docs/js/amazon_ail_type_in_search_field_windows8.js
+++ docs/js/amazon_ail_type_in_search_field_windows8.js
@@ -691,5 +691,6 @@ var amazon_ail_type_in_search_field_windows8 = {
 			[Date.UTC(2017, 8, 28), 11.11],
 			[Date.UTC(2017, 8, 28), 22.22],
 			[Date.UTC(2017, 8, 28), 22.22],
+			[Date.UTC(2017, 8, 28), 22.22],
 		]    }]
 }
--- docs/js/facebook_ail_scroll_home_1_txt_windows10.js
+++ docs/js/facebook_ail_scroll_home_1_txt_windows10.js
@@ -280,6 +280,7 @@ var facebook_ail_scroll_home_1_txt_windows10 = {
 			[Date.UTC(2017, 8, 28), 55.56],
 			[Date.UTC(2017, 8, 28), 55.56],
 			[Date.UTC(2017, 8, 28), 44.44],
+			[Date.UTC(2017, 8, 28), 55.56],
 		]    }, {
 		name: 'chrome',
 		data: [
