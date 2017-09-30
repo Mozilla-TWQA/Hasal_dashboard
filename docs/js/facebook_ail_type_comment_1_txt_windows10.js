0bd17e3bd8f1d39bec119580087ddff1889bd315
MarkYan <mark11yan@gmail.com>
2017-10-01 03:49:52 +0800

    auto deploy on 03:49:52

--- docs/js/facebook_ail_type_comment_1_txt_windows10.js
+++ docs/js/facebook_ail_type_comment_1_txt_windows10.js
@@ -275,6 +275,7 @@ var facebook_ail_type_comment_1_txt_windows10 = {
 			[Date.UTC(2017, 8, 30), 33.33],
 			[Date.UTC(2017, 8, 30), 33.33],
 			[Date.UTC(2017, 8, 30), 33.33],
+			[Date.UTC(2017, 8, 30), 33.33],
 			[Date.UTC(2017, 8, 30), 44.44],
 			[Date.UTC(2017, 8, 30), 66.67],
 		]    }, {
@@ -480,6 +481,7 @@ var facebook_ail_type_comment_1_txt_windows10 = {
 			[Date.UTC(2017, 8, 29), 5.56],
 			[Date.UTC(2017, 8, 29), 5.56],
 			[Date.UTC(2017, 8, 29), 5.56],
+			[Date.UTC(2017, 8, 30), 11.11],
 			[Date.UTC(2017, 8, 30), 11.11],
 			[Date.UTC(2017, 8, 30), 22.22],
 			[Date.UTC(2017, 8, 30), 22.22],
--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
@@ -546,6 +546,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 8, 29), 133.33],
 			[Date.UTC(2017, 8, 30), 100.0],
 			[Date.UTC(2017, 8, 30), 100.0],
+			[Date.UTC(2017, 8, 30), 100.0],
 			[Date.UTC(2017, 8, 30), 111.11],
 			[Date.UTC(2017, 8, 30), 122.22],
 			[Date.UTC(2017, 8, 30), 122.22],
