2b279f1319138975be61fd0e32f980f85431e01d
MarkYan <mark11yan@gmail.com>
2017-09-30 15:45:05 +0800

    auto deploy on 15:45:05

--- docs/js/gmail_ail_open_mail_windows10.js
+++ docs/js/gmail_ail_open_mail_windows10.js
@@ -538,6 +538,7 @@ var gmail_ail_open_mail_windows10 = {
 			[Date.UTC(2017, 8, 29), 244.44],
 			[Date.UTC(2017, 8, 29), 166.67],
 			[Date.UTC(2017, 8, 29), 166.67],
+			[Date.UTC(2017, 8, 29), 166.67],
 			[Date.UTC(2017, 8, 29), 300.0],
 		]    }]
 }
