fc4a851c35ef965944b91aafbebee2f2096f9cad
MarkYan <mark11yan@gmail.com>
2017-10-01 17:00:54 +0800

    auto deploy on 17:00:54

--- docs/js/gsearch_ail_type_searchbox_windows10.js
+++ docs/js/gsearch_ail_type_searchbox_windows10.js
@@ -226,6 +226,7 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 8, 30), 5.56],
 			[Date.UTC(2017, 8, 30), 5.56],
 			[Date.UTC(2017, 8, 30), 5.56],
+			[Date.UTC(2017, 8, 30), 5.56],
 		]    }, {
 		name: 'chrome',
 		data: [
--- docs/js/youtube_ail_type_in_search_field_windows10.js
+++ docs/js/youtube_ail_type_in_search_field_windows10.js
@@ -246,6 +246,7 @@ var youtube_ail_type_in_search_field_windows10 = {
 			[Date.UTC(2017, 8, 30), 5.56],
 			[Date.UTC(2017, 8, 30), 5.56],
 			[Date.UTC(2017, 8, 30), 5.56],
+			[Date.UTC(2017, 8, 30), 5.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -448,6 +449,7 @@ var youtube_ail_type_in_search_field_windows10 = {
 			[Date.UTC(2017, 8, 29), 5.56],
 			[Date.UTC(2017, 8, 29), 5.56],
 			[Date.UTC(2017, 8, 30), 22.22],
+			[Date.UTC(2017, 8, 30), 5.56],
 			[Date.UTC(2017, 8, 30), 5.56],
 			[Date.UTC(2017, 8, 30), 5.56],
 			[Date.UTC(2017, 8, 30), 5.56],
