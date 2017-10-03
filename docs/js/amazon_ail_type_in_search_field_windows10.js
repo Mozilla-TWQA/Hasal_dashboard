bf3090b34bdfb59e12e2a333cbcd650b84447f36
MarkYan <mark11yan@gmail.com>
2017-10-03 13:00:27 +0800

    auto deploy on 13:00:27

--- docs/js/amazon_ail_type_in_search_field_windows10.js
+++ docs/js/amazon_ail_type_in_search_field_windows10.js
@@ -255,6 +255,7 @@ var amazon_ail_type_in_search_field_windows10 = {
 			[Date.UTC(2017, 9, 2), 22.22],
 			[Date.UTC(2017, 9, 2), 22.22],
 			[Date.UTC(2017, 9, 2), 33.33],
+			[Date.UTC(2017, 9, 2), 5.56],
 		]    }, {
 		name: 'chrome',
 		data: [
