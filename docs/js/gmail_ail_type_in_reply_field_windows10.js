0330a447fd35ee70274234be64a8465940d3fc24
MarkYan <mark11yan@gmail.com>
2017-10-05 15:28:59 +0800

    auto deploy on 15:28:59

--- docs/js/gmail_ail_type_in_reply_field_windows10.js
+++ docs/js/gmail_ail_type_in_reply_field_windows10.js
@@ -246,6 +246,7 @@ var gmail_ail_type_in_reply_field_windows10 = {
 			[Date.UTC(2017, 9, 4), 11.11],
 			[Date.UTC(2017, 9, 4), 11.11],
 			[Date.UTC(2017, 9, 4), 11.11],
+			[Date.UTC(2017, 9, 4), 11.11],
 			[Date.UTC(2017, 9, 4), 22.22],
 			[Date.UTC(2017, 9, 4), 22.22],
 		]    }, {
