6515ee57dc1735a66666777cb57afae0d040abe9
MarkYan <mark11yan@gmail.com>
2017-10-06 01:18:06 +0800

    auto deploy on 01:18:06

--- docs/js/gmail_ail_reply_mail_windows10.js
+++ docs/js/gmail_ail_reply_mail_windows10.js
@@ -238,6 +238,7 @@ var gmail_ail_reply_mail_windows10 = {
 			[Date.UTC(2017, 9, 5), 233.33],
 			[Date.UTC(2017, 9, 5), 233.33],
 			[Date.UTC(2017, 9, 5), 233.33],
+			[Date.UTC(2017, 9, 5), 244.44],
 			[Date.UTC(2017, 9, 5), 255.56],
 			[Date.UTC(2017, 9, 5), 255.56],
 		]    }, {
@@ -443,6 +444,7 @@ var gmail_ail_reply_mail_windows10 = {
 			[Date.UTC(2017, 9, 5), 100.0],
 			[Date.UTC(2017, 9, 5), 100.0],
 			[Date.UTC(2017, 9, 5), 100.0],
+			[Date.UTC(2017, 9, 5), 111.11],
 			[Date.UTC(2017, 9, 5), 122.22],
 			[Date.UTC(2017, 9, 5), 122.22],
 			[Date.UTC(2017, 9, 5), 122.22],
--- docs/js/gsearch_ail_select_image_cat_windows10.js
+++ docs/js/gsearch_ail_select_image_cat_windows10.js
@@ -245,6 +245,7 @@ var gsearch_ail_select_image_cat_windows10 = {
 			[Date.UTC(2017, 9, 5), 155.56],
 			[Date.UTC(2017, 9, 5), 66.67],
 			[Date.UTC(2017, 9, 5), 66.67],
+			[Date.UTC(2017, 9, 5), 66.67],
 			[Date.UTC(2017, 9, 5), 77.78],
 			[Date.UTC(2017, 9, 5), 88.89],
 		]    }, {
