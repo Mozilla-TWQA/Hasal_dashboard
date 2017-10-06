73481d14409e0409881fc9e7783c318324ebb965
MarkYan <mark11yan@gmail.com>
2017-10-07 07:08:06 +0800

    auto deploy on 07:08:06

--- docs/js/gmail_ail_reply_mail_windows8.js
+++ docs/js/gmail_ail_reply_mail_windows8.js
@@ -374,6 +374,7 @@ var gmail_ail_reply_mail_windows8 = {
 			[Date.UTC(2017, 9, 5), 177.78],
 			[Date.UTC(2017, 9, 5), 177.78],
 			[Date.UTC(2017, 9, 5), 177.78],
+			[Date.UTC(2017, 9, 5), 177.78],
 			[Date.UTC(2017, 9, 5), 188.89],
 		]    }]
 }
