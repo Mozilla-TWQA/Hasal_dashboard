1c60f696668750b9f2b99e0447548193f85ceb2b
MarkYan <mark11yan@gmail.com>
2017-10-06 13:45:45 +0800

    auto deploy on 13:45:45

--- docs/js/gsearch_ail_type_searchbox_windows10.js
+++ docs/js/gsearch_ail_type_searchbox_windows10.js
@@ -239,6 +239,7 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 9, 5), 11.11],
 			[Date.UTC(2017, 9, 5), 11.11],
 			[Date.UTC(2017, 9, 5), 5.56],
+			[Date.UTC(2017, 9, 5), 5.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -400,6 +401,7 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 9, 4), 5.56],
 			[Date.UTC(2017, 9, 5), 33.33],
 			[Date.UTC(2017, 9, 5), 33.33],
+			[Date.UTC(2017, 9, 5), 5.56],
 			[Date.UTC(2017, 9, 5), 5.56],
 			[Date.UTC(2017, 9, 5), 5.56],
 			[Date.UTC(2017, 9, 5), 5.56],
--- docs/js/youtube_ail_select_search_suggestion_windows10.js
+++ docs/js/youtube_ail_select_search_suggestion_windows10.js
@@ -259,6 +259,7 @@ var youtube_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 9, 5), 22.22],
 			[Date.UTC(2017, 9, 5), 33.33],
 			[Date.UTC(2017, 9, 5), 33.33],
+			[Date.UTC(2017, 9, 5), 33.33],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -468,6 +469,7 @@ var youtube_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 9, 5), 5.56],
 			[Date.UTC(2017, 9, 5), 22.22],
 			[Date.UTC(2017, 9, 5), 22.22],
+			[Date.UTC(2017, 9, 5), 5.56],
 			[Date.UTC(2017, 9, 5), 5.56],
 			[Date.UTC(2017, 9, 5), 5.56],
 		]    }]
