3cee720ab5817f9c7bd1c0010c7f9b4db4cb4640
MarkYan <mark11yan@gmail.com>
2017-10-07 06:16:35 +0800

    auto deploy on 06:16:35

--- docs/js/facebook_ail_scroll_home_1_txt_windows8.js
+++ docs/js/facebook_ail_scroll_home_1_txt_windows8.js
@@ -225,6 +225,7 @@ var facebook_ail_scroll_home_1_txt_windows8 = {
 			[Date.UTC(2017, 9, 5), 55.56],
 			[Date.UTC(2017, 9, 5), 55.56],
 			[Date.UTC(2017, 9, 5), 66.67],
+			[Date.UTC(2017, 9, 5), 66.67],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -399,6 +400,7 @@ var facebook_ail_scroll_home_1_txt_windows8 = {
 			[Date.UTC(2017, 9, 5), 33.33],
 			[Date.UTC(2017, 9, 5), 33.33],
 			[Date.UTC(2017, 9, 5), 33.33],
+			[Date.UTC(2017, 9, 5), 44.44],
 			[Date.UTC(2017, 9, 5), 44.44],
 			[Date.UTC(2017, 9, 5), 44.44],
 		]    }]
--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows8.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows8.js
@@ -203,6 +203,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows8 = {
 			[Date.UTC(2017, 9, 5), 233.33],
 			[Date.UTC(2017, 9, 5), 244.44],
 			[Date.UTC(2017, 9, 5), 244.44],
+			[Date.UTC(2017, 9, 5), 244.44],
 			[Date.UTC(2017, 9, 5), 255.56],
 		]    }, {
 		name: 'chrome',
