08af4271789544d863362cb526b11f19791f0c60
MarkYan <mark11yan@gmail.com>
2017-10-07 02:30:13 +0800

    auto deploy on 02:30:13

--- docs/js/gdoc_ail_pagedown_10_text_windows8.js
+++ docs/js/gdoc_ail_pagedown_10_text_windows8.js
@@ -167,6 +167,7 @@ var gdoc_ail_pagedown_10_text_windows8 = {
 			[Date.UTC(2017, 9, 5), 55.56],
 			[Date.UTC(2017, 9, 5), 55.56],
 			[Date.UTC(2017, 9, 5), 55.56],
+			[Date.UTC(2017, 9, 5), 55.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -299,6 +300,7 @@ var gdoc_ail_pagedown_10_text_windows8 = {
 			[Date.UTC(2017, 9, 5), 44.44],
 			[Date.UTC(2017, 9, 5), 55.56],
 			[Date.UTC(2017, 9, 5), 122.22],
+			[Date.UTC(2017, 9, 5), 44.44],
 			[Date.UTC(2017, 9, 5), 44.44],
 			[Date.UTC(2017, 9, 5), 66.67],
 			[Date.UTC(2017, 9, 5), 66.67],
--- docs/js/youtube_ail_type_in_search_field_windows8.js
+++ docs/js/youtube_ail_type_in_search_field_windows8.js
@@ -147,6 +147,7 @@ var youtube_ail_type_in_search_field_windows8 = {
 			[Date.UTC(2017, 9, 5), 33.33],
 			[Date.UTC(2017, 9, 5), 33.33],
 			[Date.UTC(2017, 9, 5), 33.33],
+			[Date.UTC(2017, 9, 5), 33.33],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -249,5 +250,6 @@ var youtube_ail_type_in_search_field_windows8 = {
 			[Date.UTC(2017, 9, 5), 11.11],
 			[Date.UTC(2017, 9, 5), 22.22],
 			[Date.UTC(2017, 9, 5), 22.22],
+			[Date.UTC(2017, 9, 5), 33.33],
 		]    }]
 }
