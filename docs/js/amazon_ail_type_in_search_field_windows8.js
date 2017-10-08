7e0527eeff8cd8519fee14a5f99e02d666160c65
MarkYan <mark11yan@gmail.com>
2017-10-08 11:53:38 +0800

    auto deploy on 11:53:38

--- docs/js/amazon_ail_type_in_search_field_windows8.js
+++ docs/js/amazon_ail_type_in_search_field_windows8.js
@@ -297,6 +297,7 @@ var amazon_ail_type_in_search_field_windows8 = {
 			[Date.UTC(2017, 9, 7), 33.33],
 			[Date.UTC(2017, 9, 7), 33.33],
 			[Date.UTC(2017, 9, 7), 33.33],
+			[Date.UTC(2017, 9, 7), 33.33],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -546,6 +547,8 @@ var amazon_ail_type_in_search_field_windows8 = {
 			[Date.UTC(2017, 9, 7), 11.11],
 			[Date.UTC(2017, 9, 7), 11.11],
 			[Date.UTC(2017, 9, 7), 11.11],
+			[Date.UTC(2017, 9, 7), 11.11],
+			[Date.UTC(2017, 9, 7), 22.22],
 			[Date.UTC(2017, 9, 7), 22.22],
 			[Date.UTC(2017, 9, 7), 22.22],
 			[Date.UTC(2017, 9, 7), 22.22],
