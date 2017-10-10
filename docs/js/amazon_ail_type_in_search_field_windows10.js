25d19f78ce153d6c95b03a374b80b1363affff5b
MarkYan <mark11yan@gmail.com>
2017-10-10 15:34:23 +0800

    auto deploy on 15:34:23

--- docs/js/amazon_ail_type_in_search_field_windows10.js
+++ docs/js/amazon_ail_type_in_search_field_windows10.js
@@ -217,6 +217,7 @@ var amazon_ail_type_in_search_field_windows10 = {
 			[Date.UTC(2017, 9, 9), 5.56],
 			[Date.UTC(2017, 9, 9), 5.56],
 			[Date.UTC(2017, 9, 9), 5.56],
+			[Date.UTC(2017, 9, 9), 5.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -365,6 +366,7 @@ var amazon_ail_type_in_search_field_windows10 = {
 			[Date.UTC(2017, 9, 9), 5.56],
 			[Date.UTC(2017, 9, 9), 5.56],
 			[Date.UTC(2017, 9, 9), 5.56],
+			[Date.UTC(2017, 9, 9), 22.22],
 			[Date.UTC(2017, 9, 9), 5.56],
 			[Date.UTC(2017, 9, 9), 5.56],
 			[Date.UTC(2017, 9, 9), 5.56],
--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
@@ -439,6 +439,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 9, 9), 122.22],
 			[Date.UTC(2017, 9, 9), 133.33],
 			[Date.UTC(2017, 9, 9), 111.11],
+			[Date.UTC(2017, 9, 9), 122.22],
 			[Date.UTC(2017, 9, 9), 144.44],
 			[Date.UTC(2017, 9, 9), 155.56],
 			[Date.UTC(2017, 9, 9), 166.67],
