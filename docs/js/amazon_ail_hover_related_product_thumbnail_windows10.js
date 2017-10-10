24d163f9f5992c86fcdc9cc8cd1651a17d8bbb3c
MarkYan <mark11yan@gmail.com>
2017-10-10 10:44:32 +0800

    auto deploy on 10:44:32

--- docs/js/amazon_ail_hover_related_product_thumbnail_windows10.js
+++ docs/js/amazon_ail_hover_related_product_thumbnail_windows10.js
@@ -227,6 +227,7 @@ var amazon_ail_hover_related_product_thumbnail_windows10 = {
 			[Date.UTC(2017, 9, 9), 5.56],
 			[Date.UTC(2017, 9, 9), 5.56],
 			[Date.UTC(2017, 9, 9), 5.56],
+			[Date.UTC(2017, 9, 9), 5.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -415,6 +416,7 @@ var amazon_ail_hover_related_product_thumbnail_windows10 = {
 			[Date.UTC(2017, 9, 8), 500.0],
 			[Date.UTC(2017, 9, 8), 544.44],
 			[Date.UTC(2017, 9, 9), 433.33],
+			[Date.UTC(2017, 9, 9), 455.56],
 			[Date.UTC(2017, 9, 9), 455.56],
 			[Date.UTC(2017, 9, 9), 455.56],
 			[Date.UTC(2017, 9, 9), 466.67],
--- docs/js/facebook_ail_type_message_1_txt_windows8.js
+++ docs/js/facebook_ail_type_message_1_txt_windows8.js
@@ -266,6 +266,7 @@ var facebook_ail_type_message_1_txt_windows8 = {
 			[Date.UTC(2017, 9, 9), 44.44],
 			[Date.UTC(2017, 9, 9), 44.44],
 			[Date.UTC(2017, 9, 9), 44.44],
+			[Date.UTC(2017, 9, 9), 44.44],
 			[Date.UTC(2017, 9, 9), 55.56],
 			[Date.UTC(2017, 9, 9), 55.56],
 			[Date.UTC(2017, 9, 9), 55.56],
