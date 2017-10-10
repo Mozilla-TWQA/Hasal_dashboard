e5513e222124bc23f7e7092a28913250dcedf2bf
MarkYan <mark11yan@gmail.com>
2017-10-10 12:01:53 +0800

    auto deploy on 12:01:53

--- docs/js/facebook_ail_type_message_1_txt_windows10.js
+++ docs/js/facebook_ail_type_message_1_txt_windows10.js
@@ -223,6 +223,7 @@ var facebook_ail_type_message_1_txt_windows10 = {
 			[Date.UTC(2017, 9, 9), 66.67],
 			[Date.UTC(2017, 9, 9), 66.67],
 			[Date.UTC(2017, 9, 9), 66.67],
+			[Date.UTC(2017, 9, 9), 66.67],
 			[Date.UTC(2017, 9, 9), 55.56],
 		]    }, {
 		name: 'chrome',
