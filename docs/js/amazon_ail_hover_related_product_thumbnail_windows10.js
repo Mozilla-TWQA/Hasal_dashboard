db54754d640e923c4c2d97221c20215aa87facc4
MarkYan <mark11yan@gmail.com>
2017-10-12 01:26:20 +0800

    auto deploy on 01:26:20

--- docs/js/amazon_ail_hover_related_product_thumbnail_windows10.js
+++ docs/js/amazon_ail_hover_related_product_thumbnail_windows10.js
@@ -167,6 +167,7 @@ var amazon_ail_hover_related_product_thumbnail_windows10 = {
 			[Date.UTC(2017, 9, 5), 422.22],
 			[Date.UTC(2017, 9, 5), 422.22],
 			[Date.UTC(2017, 9, 5), 422.22],
+			[Date.UTC(2017, 9, 5), 422.22],
 			[Date.UTC(2017, 9, 5), 433.33],
 			[Date.UTC(2017, 9, 5), 433.33],
 			[Date.UTC(2017, 9, 5), 433.33],
