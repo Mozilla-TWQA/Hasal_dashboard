be19a7cf0c5d2f35e91337819b95697f0f84b78e
MarkYan <mark11yan@gmail.com>
2017-10-13 00:48:25 +0800

    auto deploy on 00:48:25

--- docs/js/amazon_ail_select_search_suggestion_windows8.js
+++ docs/js/amazon_ail_select_search_suggestion_windows8.js
@@ -619,5 +619,6 @@ var amazon_ail_select_search_suggestion_windows8 = {
 			[Date.UTC(2017, 9, 11), 22.22],
 			[Date.UTC(2017, 9, 11), 22.22],
 			[Date.UTC(2017, 9, 11), 33.33],
+			[Date.UTC(2017, 9, 12), 11.11],
 		]    }]
 }
--- docs/js/gsearch_ail_select_search_suggestion_windows8.js
+++ docs/js/gsearch_ail_select_search_suggestion_windows8.js
@@ -324,6 +324,7 @@ var gsearch_ail_select_search_suggestion_windows8 = {
 			[Date.UTC(2017, 9, 11), 33.33],
 			[Date.UTC(2017, 9, 12), 22.22],
 			[Date.UTC(2017, 9, 12), 22.22],
+			[Date.UTC(2017, 9, 12), 22.22],
 			[Date.UTC(2017, 9, 12), 33.33],
 			[Date.UTC(2017, 9, 12), 33.33],
 			[Date.UTC(2017, 9, 12), 33.33],
