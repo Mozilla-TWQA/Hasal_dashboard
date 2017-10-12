98b81ee8feef0b7f3a3e4be30cc8f019c65ee97a
MarkYan <mark11yan@gmail.com>
2017-10-12 19:26:44 +0800

    auto deploy on 19:26:44

--- docs/js/facebook_ail_type_composerbox_1_txt_windows10.js
+++ docs/js/facebook_ail_type_composerbox_1_txt_windows10.js
@@ -236,6 +236,7 @@ var facebook_ail_type_composerbox_1_txt_windows10 = {
 			[Date.UTC(2017, 8, 29), 5.56],
 			[Date.UTC(2017, 8, 29), 5.56],
 			[Date.UTC(2017, 8, 29), 5.56],
+			[Date.UTC(2017, 8, 29), 5.56],
 			[Date.UTC(2017, 8, 30), 11.11],
 			[Date.UTC(2017, 8, 30), 11.11],
 			[Date.UTC(2017, 8, 30), 11.11],
--- docs/js/gsearch_ail_type_searchbox_windows10.js
+++ docs/js/gsearch_ail_type_searchbox_windows10.js
@@ -229,6 +229,7 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 8, 28), 5.56],
 			[Date.UTC(2017, 8, 28), 5.56],
 			[Date.UTC(2017, 8, 28), 5.56],
+			[Date.UTC(2017, 8, 29), 22.22],
 			[Date.UTC(2017, 8, 29), 5.56],
 			[Date.UTC(2017, 8, 29), 5.56],
 			[Date.UTC(2017, 8, 29), 5.56],
