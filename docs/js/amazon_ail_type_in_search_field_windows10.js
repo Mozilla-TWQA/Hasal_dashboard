07e2491b09626c3d47c5fbcd57753c580ac32ad9
MarkYan <mark11yan@gmail.com>
2017-10-13 09:59:32 +0800

    auto deploy on 09:59:32

--- docs/js/amazon_ail_type_in_search_field_windows10.js
+++ docs/js/amazon_ail_type_in_search_field_windows10.js
@@ -206,6 +206,7 @@ var amazon_ail_type_in_search_field_windows10 = {
 			[Date.UTC(2017, 9, 12), 5.56],
 			[Date.UTC(2017, 9, 12), 22.22],
 			[Date.UTC(2017, 9, 12), 5.56],
+			[Date.UTC(2017, 9, 12), 5.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -371,6 +372,7 @@ var amazon_ail_type_in_search_field_windows10 = {
 			[Date.UTC(2017, 9, 11), 5.56],
 			[Date.UTC(2017, 9, 11), 5.56],
 			[Date.UTC(2017, 9, 11), 5.56],
+			[Date.UTC(2017, 9, 12), 5.56],
 			[Date.UTC(2017, 9, 12), 5.56],
 			[Date.UTC(2017, 9, 12), 5.56],
 			[Date.UTC(2017, 9, 12), 5.56],
--- docs/js/facebook_ail_type_composerbox_1_txt_windows10.js
+++ docs/js/facebook_ail_type_composerbox_1_txt_windows10.js
@@ -209,6 +209,7 @@ var facebook_ail_type_composerbox_1_txt_windows10 = {
 			[Date.UTC(2017, 9, 12), 33.33],
 			[Date.UTC(2017, 9, 12), 55.56],
 			[Date.UTC(2017, 9, 12), 33.33],
+			[Date.UTC(2017, 9, 12), 33.33],
 			[Date.UTC(2017, 9, 12), 44.44],
 			[Date.UTC(2017, 9, 12), 44.44],
 			[Date.UTC(2017, 9, 12), 55.56],
