28582d5de9c533e85c36e81da9354cbfde442c97
MarkYan <mark11yan@gmail.com>
2017-10-17 03:59:56 +0800

    auto deploy on 03:59:56

--- docs/js/facebook_ail_scroll_home_1_txt_windows10.js
+++ docs/js/facebook_ail_scroll_home_1_txt_windows10.js
@@ -509,5 +509,6 @@ var facebook_ail_scroll_home_1_txt_windows10 = {
 			[Date.UTC(2017, 9, 16), 22.22],
 			[Date.UTC(2017, 9, 16), 33.33],
 			[Date.UTC(2017, 9, 16), 33.33],
+			[Date.UTC(2017, 9, 16), 33.33],
 		]    }]
 }
