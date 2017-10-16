e3c74f9920f97f4c5d2872380b5f32797c80394f
MarkYan <mark11yan@gmail.com>
2017-10-17 03:40:25 +0800

    auto deploy on 03:40:25

--- docs/js/youtube_ail_type_in_search_field_windows10.js
+++ docs/js/youtube_ail_type_in_search_field_windows10.js
@@ -276,5 +276,6 @@ var youtube_ail_type_in_search_field_windows10 = {
 			[Date.UTC(2017, 9, 16), 11.11],
 			[Date.UTC(2017, 9, 16), 5.56],
 			[Date.UTC(2017, 9, 16), 5.56],
+			[Date.UTC(2017, 9, 16), 5.56],
 		]    }]
 }
