bf13dcc09763bd318afb98d1e8739a665bf2db7f
MarkYan <mark11yan@gmail.com>
2017-10-17 11:04:47 +0800

    auto deploy on 11:04:47

--- docs/js/facebook_ail_type_message_1_txt_windows8.js
+++ docs/js/facebook_ail_type_message_1_txt_windows8.js
@@ -303,6 +303,8 @@ var facebook_ail_type_message_1_txt_windows8 = {
 			[Date.UTC(2017, 9, 16), 33.33],
 			[Date.UTC(2017, 9, 16), 44.44],
 			[Date.UTC(2017, 9, 16), 44.44],
+			[Date.UTC(2017, 9, 16), 44.44],
+			[Date.UTC(2017, 9, 16), 55.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -532,5 +534,7 @@ var facebook_ail_type_message_1_txt_windows8 = {
 			[Date.UTC(2017, 9, 16), 22.22],
 			[Date.UTC(2017, 9, 16), 22.22],
 			[Date.UTC(2017, 9, 16), 22.22],
+			[Date.UTC(2017, 9, 16), 33.33],
+			[Date.UTC(2017, 9, 16), 33.33],
 		]    }]
 }
