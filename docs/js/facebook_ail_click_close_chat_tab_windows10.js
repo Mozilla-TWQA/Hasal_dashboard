89bb241dbe9d500799219320d6c11bbaefd8f7d7
MarkYan <mark11yan@gmail.com>
2017-10-18 16:57:42 +0800

    auto deploy on 16:57:42

--- docs/js/facebook_ail_click_close_chat_tab_windows10.js
+++ docs/js/facebook_ail_click_close_chat_tab_windows10.js
@@ -213,6 +213,7 @@ var facebook_ail_click_close_chat_tab_windows10 = {
 			[Date.UTC(2017, 9, 17), 133.33],
 			[Date.UTC(2017, 9, 17), 88.89],
 			[Date.UTC(2017, 9, 17), 66.67],
+			[Date.UTC(2017, 9, 17), 77.78],
 		]    }, {
 		name: 'chrome',
 		data: [
