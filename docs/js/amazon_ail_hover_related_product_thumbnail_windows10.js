9e20f8633c9a03916b6804a2afff51b4fe1d9f6f
MarkYan <mark11yan@gmail.com>
2017-10-19 07:28:33 +0800

    auto deploy on 07:28:33

--- docs/js/amazon_ail_hover_related_product_thumbnail_windows10.js
+++ docs/js/amazon_ail_hover_related_product_thumbnail_windows10.js
@@ -346,6 +346,7 @@ var amazon_ail_hover_related_product_thumbnail_windows10 = {
 			[Date.UTC(2017, 9, 18), 444.44],
 			[Date.UTC(2017, 9, 18), 455.56],
 			[Date.UTC(2017, 9, 18), 455.56],
+			[Date.UTC(2017, 9, 18), 466.67],
 			[Date.UTC(2017, 9, 18), 477.78],
 		]    }, {
 		name: 'chrome',
--- docs/js/facebook_ail_click_photo_viewer_right_arrow_windows10.js
+++ docs/js/facebook_ail_click_photo_viewer_right_arrow_windows10.js
@@ -184,6 +184,7 @@ var facebook_ail_click_photo_viewer_right_arrow_windows10 = {
 			[Date.UTC(2017, 9, 18), 122.22],
 			[Date.UTC(2017, 9, 18), 155.56],
 			[Date.UTC(2017, 9, 18), 166.67],
+			[Date.UTC(2017, 9, 18), 177.78],
 		]    }, {
 		name: 'chrome',
 		data: [
