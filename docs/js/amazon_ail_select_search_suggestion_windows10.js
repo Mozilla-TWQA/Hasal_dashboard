b84e4452b17308c0d386e978de9182a2155392e8
MarkYan <mark11yan@gmail.com>
2017-10-19 05:30:54 +0800

    auto deploy on 05:30:54

--- docs/js/amazon_ail_select_search_suggestion_windows10.js
+++ docs/js/amazon_ail_select_search_suggestion_windows10.js
@@ -243,6 +243,7 @@ var amazon_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 9, 18), 11.11],
 			[Date.UTC(2017, 9, 18), 11.11],
 			[Date.UTC(2017, 9, 18), 11.11],
+			[Date.UTC(2017, 9, 18), 22.22],
 			[Date.UTC(2017, 9, 18), 33.33],
 		]    }, {
 		name: 'chrome',
