b0577a6dcdfb80384969e7d1a6c3a91258edd3c8
MarkYan <mark11yan@gmail.com>
2017-10-18 19:28:35 +0800

    auto deploy on 19:28:35

--- docs/js/gdoc_ail_pagedown_10_text_windows10.js
+++ docs/js/gdoc_ail_pagedown_10_text_windows10.js
@@ -200,6 +200,7 @@ var gdoc_ail_pagedown_10_text_windows10 = {
 			[Date.UTC(2017, 9, 17), 33.33],
 			[Date.UTC(2017, 9, 17), 66.67],
 			[Date.UTC(2017, 9, 17), 66.67],
+			[Date.UTC(2017, 9, 17), 66.67],
 		]    }, {
 		name: 'chrome',
 		data: [
--- docs/js/youtube_ail_type_in_search_field_windows10.js
+++ docs/js/youtube_ail_type_in_search_field_windows10.js
@@ -243,6 +243,7 @@ var youtube_ail_type_in_search_field_windows10 = {
 			[Date.UTC(2017, 9, 14), 5.56],
 			[Date.UTC(2017, 9, 14), 5.56],
 			[Date.UTC(2017, 9, 14), 5.56],
+			[Date.UTC(2017, 9, 14), 5.56],
 			[Date.UTC(2017, 9, 15), 11.11],
 			[Date.UTC(2017, 9, 15), 11.11],
 			[Date.UTC(2017, 9, 15), 11.11],
