8667969246969ef8e38f20b10f5855f8ce42224b
MarkYan <mark11yan@gmail.com>
2017-10-20 16:45:38 +0800

    auto deploy on 16:45:38

--- docs/js/facebook_ail_type_composerbox_1_txt_windows10.js
+++ docs/js/facebook_ail_type_composerbox_1_txt_windows10.js
@@ -329,6 +329,7 @@ var facebook_ail_type_composerbox_1_txt_windows10 = {
 			[Date.UTC(2017, 9, 19), 22.22],
 			[Date.UTC(2017, 9, 19), 5.56],
 			[Date.UTC(2017, 9, 19), 11.11],
+			[Date.UTC(2017, 9, 19), 22.22],
 			[Date.UTC(2017, 9, 19), 5.56],
 		]    }]
 }
--- docs/js/youtube_ail_type_in_search_field_windows10.js
+++ docs/js/youtube_ail_type_in_search_field_windows10.js
@@ -258,5 +258,7 @@ var youtube_ail_type_in_search_field_windows10 = {
 			[Date.UTC(2017, 9, 19), 5.56],
 			[Date.UTC(2017, 9, 19), 5.56],
 			[Date.UTC(2017, 9, 19), 5.56],
+			[Date.UTC(2017, 9, 19), 5.56],
+			[Date.UTC(2017, 9, 19), 5.56],
 		]    }]
 }
