ec2ee8132788c04615c41b0a5ce7246c718c00d2
MarkYan <mark11yan@gmail.com>
2017-10-24 01:58:28 +0800

    auto deploy on 01:58:28

--- docs/js/facebook_ail_type_message_1_txt_windows10.js
+++ docs/js/facebook_ail_type_message_1_txt_windows10.js
@@ -241,6 +241,7 @@ var facebook_ail_type_message_1_txt_windows10 = {
 			[Date.UTC(2017, 9, 23), 33.33],
 			[Date.UTC(2017, 9, 23), 33.33],
 			[Date.UTC(2017, 9, 23), 44.44],
+			[Date.UTC(2017, 9, 23), 44.44],
 			[Date.UTC(2017, 9, 23), 66.67],
 			[Date.UTC(2017, 9, 23), 66.67],
 		]    }, {
@@ -367,6 +368,7 @@ var facebook_ail_type_message_1_txt_windows10 = {
 			[Date.UTC(2017, 9, 22), 5.56],
 			[Date.UTC(2017, 9, 22), 5.56],
 			[Date.UTC(2017, 9, 23), 22.22],
+			[Date.UTC(2017, 9, 23), 5.56],
 			[Date.UTC(2017, 9, 23), 5.56],
 			[Date.UTC(2017, 9, 23), 5.56],
 			[Date.UTC(2017, 9, 23), 5.56],
--- docs/js/gmail_ail_type_in_reply_field_windows10.js
+++ docs/js/gmail_ail_type_in_reply_field_windows10.js
@@ -161,6 +161,7 @@ var gmail_ail_type_in_reply_field_windows10 = {
 			[Date.UTC(2017, 9, 23), 22.22],
 			[Date.UTC(2017, 9, 23), 33.33],
 			[Date.UTC(2017, 9, 23), 33.33],
+			[Date.UTC(2017, 9, 23), 33.33],
 			[Date.UTC(2017, 9, 23), 44.44],
 		]    }, {
 		name: 'chrome',
@@ -282,6 +283,7 @@ var gmail_ail_type_in_reply_field_windows10 = {
 			[Date.UTC(2017, 9, 22), 5.56],
 			[Date.UTC(2017, 9, 22), 5.56],
 			[Date.UTC(2017, 9, 22), 5.56],
+			[Date.UTC(2017, 9, 23), 11.11],
 			[Date.UTC(2017, 9, 23), 11.11],
 			[Date.UTC(2017, 9, 23), 11.11],
 			[Date.UTC(2017, 9, 23), 11.11],
