7c47a3f7674508fa799559aec81c49505cc7bd74
MarkYan <mark11yan@gmail.com>
2017-10-24 00:54:22 +0800

    auto deploy on 00:54:22

--- docs/js/gmail_ail_type_in_reply_field_windows8.js
+++ docs/js/gmail_ail_type_in_reply_field_windows8.js
@@ -263,6 +263,7 @@ var gmail_ail_type_in_reply_field_windows8 = {
 			[Date.UTC(2017, 9, 23), 22.22],
 			[Date.UTC(2017, 9, 23), 22.22],
 			[Date.UTC(2017, 9, 23), 22.22],
+			[Date.UTC(2017, 9, 23), 22.22],
 			[Date.UTC(2017, 9, 23), 33.33],
 			[Date.UTC(2017, 9, 23), 33.33],
 		]    }]
--- docs/js/gsearch_ail_select_image_cat_windows8.js
+++ docs/js/gsearch_ail_select_image_cat_windows8.js
@@ -262,6 +262,7 @@ var gsearch_ail_select_image_cat_windows8 = {
 			[Date.UTC(2017, 9, 22), 88.89],
 			[Date.UTC(2017, 9, 23), 122.22],
 			[Date.UTC(2017, 9, 23), 122.22],
+			[Date.UTC(2017, 9, 23), 133.33],
 			[Date.UTC(2017, 9, 23), 144.44],
 			[Date.UTC(2017, 9, 23), 88.89],
 			[Date.UTC(2017, 9, 23), 88.89],
