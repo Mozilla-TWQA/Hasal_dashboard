80f78a7fb5194df3dc06ff6ff4c4148cf8728a9e
MarkYan <mark11yan@gmail.com>
2017-10-25 13:36:02 +0800

    auto deploy on 13:36:02

--- docs/js/amazon_ail_type_in_search_field_windows8.js
+++ docs/js/amazon_ail_type_in_search_field_windows8.js
@@ -232,6 +232,7 @@ var amazon_ail_type_in_search_field_windows8 = {
 			[Date.UTC(2017, 9, 24), 33.33],
 			[Date.UTC(2017, 9, 24), 33.33],
 			[Date.UTC(2017, 9, 24), 33.33],
+			[Date.UTC(2017, 9, 24), 22.22],
 			[Date.UTC(2017, 9, 24), 33.33],
 			[Date.UTC(2017, 9, 24), 33.33],
 		]    }, {
@@ -426,5 +427,6 @@ var amazon_ail_type_in_search_field_windows8 = {
 			[Date.UTC(2017, 9, 24), 5.56],
 			[Date.UTC(2017, 9, 24), 11.11],
 			[Date.UTC(2017, 9, 24), 11.11],
+			[Date.UTC(2017, 9, 24), 22.22],
 		]    }]
 }
--- docs/js/facebook_ail_click_open_chat_tab_emoji_windows10.js
+++ docs/js/facebook_ail_click_open_chat_tab_emoji_windows10.js
@@ -417,6 +417,8 @@ var facebook_ail_click_open_chat_tab_emoji_windows10 = {
 			[Date.UTC(2017, 9, 24), 133.33],
 			[Date.UTC(2017, 9, 24), 55.56],
 			[Date.UTC(2017, 9, 24), 55.56],
+			[Date.UTC(2017, 9, 24), 55.56],
+			[Date.UTC(2017, 9, 24), 66.67],
 			[Date.UTC(2017, 9, 24), 66.67],
 			[Date.UTC(2017, 9, 24), 66.67],
 			[Date.UTC(2017, 9, 24), 88.89],
