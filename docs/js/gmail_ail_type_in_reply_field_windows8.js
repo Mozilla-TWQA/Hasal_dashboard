166e976d2ac5c90f0f5838ee091dbd5dc37c527b
MarkYan <mark11yan@gmail.com>
2017-10-27 13:40:42 +0800

    auto deploy on 13:40:42

--- docs/js/gmail_ail_type_in_reply_field_windows8.js
+++ docs/js/gmail_ail_type_in_reply_field_windows8.js
@@ -159,6 +159,7 @@ var gmail_ail_type_in_reply_field_windows8 = {
 			[Date.UTC(2017, 9, 26), 33.33],
 			[Date.UTC(2017, 9, 26), 33.33],
 			[Date.UTC(2017, 9, 26), 44.44],
+			[Date.UTC(2017, 9, 26), 44.44],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -279,6 +280,7 @@ var gmail_ail_type_in_reply_field_windows8 = {
 			[Date.UTC(2017, 9, 26), 33.33],
 			[Date.UTC(2017, 9, 26), 44.44],
 			[Date.UTC(2017, 9, 26), 44.44],
+			[Date.UTC(2017, 9, 26), 22.22],
 			[Date.UTC(2017, 9, 26), 22.22],
 			[Date.UTC(2017, 9, 26), 22.22],
 			[Date.UTC(2017, 9, 26), 22.22],
--- docs/js/gsearch_ail_type_searchbox_windows8.js
+++ docs/js/gsearch_ail_type_searchbox_windows8.js
@@ -165,6 +165,8 @@ var gsearch_ail_type_searchbox_windows8 = {
 			[Date.UTC(2017, 9, 26), 33.33],
 			[Date.UTC(2017, 9, 26), 33.33],
 			[Date.UTC(2017, 9, 26), 22.22],
+			[Date.UTC(2017, 9, 26), 22.22],
+			[Date.UTC(2017, 9, 26), 22.22],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -294,6 +296,7 @@ var gsearch_ail_type_searchbox_windows8 = {
 			[Date.UTC(2017, 9, 26), 22.22],
 			[Date.UTC(2017, 9, 26), 22.22],
 			[Date.UTC(2017, 9, 26), 22.22],
+			[Date.UTC(2017, 9, 26), 11.11],
 			[Date.UTC(2017, 9, 26), 11.11],
 			[Date.UTC(2017, 9, 26), 22.22],
 		]    }]
