2c755dd65e343110e57dd868e08b032839f9a86b
MarkYan <mark11yan@gmail.com>
2017-10-29 13:36:21 +0800

    auto deploy on 13:36:21

--- docs/js/facebook_ail_type_composerbox_1_txt_windows10.js
+++ docs/js/facebook_ail_type_composerbox_1_txt_windows10.js
@@ -184,6 +184,7 @@ var facebook_ail_type_composerbox_1_txt_windows10 = {
 			[Date.UTC(2017, 9, 28), 55.56],
 			[Date.UTC(2017, 9, 28), 33.33],
 			[Date.UTC(2017, 9, 28), 33.33],
+			[Date.UTC(2017, 9, 28), 44.44],
 			[Date.UTC(2017, 9, 28), 55.56],
 		]    }, {
 		name: 'chrome',
@@ -298,6 +299,7 @@ var facebook_ail_type_composerbox_1_txt_windows10 = {
 			[Date.UTC(2017, 9, 28), 5.56],
 			[Date.UTC(2017, 9, 28), 5.56],
 			[Date.UTC(2017, 9, 28), 5.56],
+			[Date.UTC(2017, 9, 28), 11.11],
 			[Date.UTC(2017, 9, 28), 22.22],
 			[Date.UTC(2017, 9, 28), 5.56],
 			[Date.UTC(2017, 9, 28), 5.56],
