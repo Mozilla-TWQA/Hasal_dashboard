1af36bf14a8de8509c936e9e9d8999c85d55d0e8
MarkYan <mark11yan@gmail.com>
2017-10-30 05:24:24 +0800

    auto deploy on 05:24:24

--- docs/js/facebook_ail_click_photo_viewer_right_arrow_windows10.js
+++ docs/js/facebook_ail_click_photo_viewer_right_arrow_windows10.js
@@ -364,5 +364,6 @@ var facebook_ail_click_photo_viewer_right_arrow_windows10 = {
 			[Date.UTC(2017, 9, 29), 100.0],
 			[Date.UTC(2017, 9, 29), 100.0],
 			[Date.UTC(2017, 9, 29), 77.78],
+			[Date.UTC(2017, 9, 29), 88.89],
 		]    }]
 }
