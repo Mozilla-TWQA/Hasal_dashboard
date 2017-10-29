fc0edc2896d1802004cc247541d3a7cccefddbbd
MarkYan <mark11yan@gmail.com>
2017-10-29 13:17:19 +0800

    auto deploy on 13:17:19

--- docs/js/gsearch_ail_select_image_cat_windows10.js
+++ docs/js/gsearch_ail_select_image_cat_windows10.js
@@ -164,6 +164,7 @@ var gsearch_ail_select_image_cat_windows10 = {
 			[Date.UTC(2017, 9, 28), 155.56],
 			[Date.UTC(2017, 9, 28), 100.0],
 			[Date.UTC(2017, 9, 28), 111.11],
+			[Date.UTC(2017, 9, 28), 122.22],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -283,6 +284,7 @@ var gsearch_ail_select_image_cat_windows10 = {
 			[Date.UTC(2017, 9, 28), 344.44],
 			[Date.UTC(2017, 9, 28), 166.67],
 			[Date.UTC(2017, 9, 28), 166.67],
+			[Date.UTC(2017, 9, 28), 177.78],
 			[Date.UTC(2017, 9, 28), 177.78],
 		]    }]
 }
--- docs/js/youtube_ail_select_search_suggestion_windows10.js
+++ docs/js/youtube_ail_select_search_suggestion_windows10.js
@@ -159,6 +159,7 @@ var youtube_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 9, 28), 33.33],
 			[Date.UTC(2017, 9, 28), 5.56],
 			[Date.UTC(2017, 9, 28), 5.56],
+			[Date.UTC(2017, 9, 28), 22.22],
 			[Date.UTC(2017, 9, 28), 33.33],
 		]    }, {
 		name: 'chrome',
@@ -276,6 +277,7 @@ var youtube_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 9, 28), 5.56],
 			[Date.UTC(2017, 9, 28), 5.56],
 			[Date.UTC(2017, 9, 28), 5.56],
+			[Date.UTC(2017, 9, 28), 11.11],
 			[Date.UTC(2017, 9, 28), 5.56],
 		]    }]
 }
