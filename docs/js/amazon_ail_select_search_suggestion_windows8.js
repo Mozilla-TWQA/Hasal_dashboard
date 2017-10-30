10f8dac6dda4e941b7f600f5037558013975b38c
MarkYan <mark11yan@gmail.com>
2017-10-31 01:37:49 +0800

    auto deploy on 01:37:49

--- docs/js/amazon_ail_select_search_suggestion_windows8.js
+++ docs/js/amazon_ail_select_search_suggestion_windows8.js
@@ -198,6 +198,7 @@ var amazon_ail_select_search_suggestion_windows8 = {
 			[Date.UTC(2017, 9, 29), 33.33],
 			[Date.UTC(2017, 9, 29), 44.44],
 			[Date.UTC(2017, 9, 29), 11.11],
+			[Date.UTC(2017, 9, 29), 11.11],
 			[Date.UTC(2017, 9, 29), 22.22],
 			[Date.UTC(2017, 9, 29), 22.22],
 			[Date.UTC(2017, 9, 29), 22.22],
