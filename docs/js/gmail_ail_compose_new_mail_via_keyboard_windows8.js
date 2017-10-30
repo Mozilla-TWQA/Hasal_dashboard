7cd488c6057a11e34810e3fbe3951a1a4a20bff7
MarkYan <mark11yan@gmail.com>
2017-10-31 06:37:54 +0800

    auto deploy on 06:37:54

--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows8.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows8.js
@@ -252,6 +252,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows8 = {
 			[Date.UTC(2017, 9, 29), 255.56],
 			[Date.UTC(2017, 9, 29), 255.56],
 			[Date.UTC(2017, 9, 29), 255.56],
+			[Date.UTC(2017, 9, 30), 222.22],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -444,6 +445,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows8 = {
 			[Date.UTC(2017, 9, 29), 155.56],
 			[Date.UTC(2017, 9, 29), 155.56],
 			[Date.UTC(2017, 9, 29), 155.56],
+			[Date.UTC(2017, 9, 30), 122.22],
 			[Date.UTC(2017, 9, 30), 122.22],
 			[Date.UTC(2017, 9, 30), 122.22],
 		]    }]
--- docs/js/youtube_ail_select_search_suggestion_windows8.js
+++ docs/js/youtube_ail_select_search_suggestion_windows8.js
@@ -353,5 +353,6 @@ var youtube_ail_select_search_suggestion_windows8 = {
 			[Date.UTC(2017, 9, 29), 33.33],
 			[Date.UTC(2017, 9, 29), 33.33],
 			[Date.UTC(2017, 9, 29), 33.33],
+			[Date.UTC(2017, 9, 30), 22.22],
 		]    }]
 }
