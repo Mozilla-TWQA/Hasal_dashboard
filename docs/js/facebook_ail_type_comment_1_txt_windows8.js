fc6e78f6c37e368b5434372974d4b4d084c1d62c
MarkYan <mark11yan@gmail.com>
2017-10-31 12:02:34 +0800

    auto deploy on 12:02:34

--- docs/js/facebook_ail_type_comment_1_txt_windows8.js
+++ docs/js/facebook_ail_type_comment_1_txt_windows8.js
@@ -151,6 +151,7 @@ var facebook_ail_type_comment_1_txt_windows8 = {
 			[Date.UTC(2017, 9, 28), 44.44],
 			[Date.UTC(2017, 9, 28), 44.44],
 			[Date.UTC(2017, 9, 28), 44.44],
+			[Date.UTC(2017, 9, 28), 44.44],
 			[Date.UTC(2017, 9, 28), 55.56],
 			[Date.UTC(2017, 9, 29), 44.44],
 			[Date.UTC(2017, 9, 29), 44.44],
--- docs/js/gsearch_ail_select_image_cat_windows10.js
+++ docs/js/gsearch_ail_select_image_cat_windows10.js
@@ -294,6 +294,7 @@ var gsearch_ail_select_image_cat_windows10 = {
 			[Date.UTC(2017, 9, 29), 188.89],
 			[Date.UTC(2017, 9, 29), 188.89],
 			[Date.UTC(2017, 9, 29), 188.89],
+			[Date.UTC(2017, 9, 30), 144.44],
 			[Date.UTC(2017, 9, 30), 155.56],
 			[Date.UTC(2017, 9, 30), 166.67],
 			[Date.UTC(2017, 9, 30), 177.78],
