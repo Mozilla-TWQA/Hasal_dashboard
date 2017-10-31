ddaf5e5a3a5902c9183fddeedf06029d38ef22fc
MarkYan <mark11yan@gmail.com>
2017-10-31 09:36:08 +0800

    auto deploy on 09:36:08

--- docs/js/facebook_ail_click_open_chat_tab_emoji_windows10.js
+++ docs/js/facebook_ail_click_open_chat_tab_emoji_windows10.js
@@ -386,6 +386,7 @@ var facebook_ail_click_open_chat_tab_emoji_windows10 = {
 			[Date.UTC(2017, 9, 29), 66.67],
 			[Date.UTC(2017, 9, 29), 88.89],
 			[Date.UTC(2017, 9, 29), 88.89],
+			[Date.UTC(2017, 9, 30), 100.0],
 			[Date.UTC(2017, 9, 30), 66.67],
 		]    }]
 }
