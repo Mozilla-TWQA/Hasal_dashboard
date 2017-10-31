edcdd17342a511a47ebcfc819e215449893638df
MarkYan <mark11yan@gmail.com>
2017-11-01 04:02:42 +0800

    auto deploy on 04:02:42

--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows8.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows8.js
@@ -259,6 +259,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows8 = {
 			[Date.UTC(2017, 9, 30), 244.44],
 			[Date.UTC(2017, 9, 30), 244.44],
 			[Date.UTC(2017, 9, 30), 244.44],
+			[Date.UTC(2017, 9, 30), 255.56],
 			[Date.UTC(2017, 9, 30), 266.67],
 			[Date.UTC(2017, 9, 30), 266.67],
 		]    }, {
--- docs/js/gmail_ail_reply_mail_windows8.js
+++ docs/js/gmail_ail_reply_mail_windows8.js
@@ -220,6 +220,7 @@ var gmail_ail_reply_mail_windows8 = {
 			[Date.UTC(2017, 9, 30), 266.67],
 			[Date.UTC(2017, 9, 30), 266.67],
 			[Date.UTC(2017, 9, 30), 266.67],
+			[Date.UTC(2017, 9, 30), 266.67],
 			[Date.UTC(2017, 9, 30), 288.89],
 		]    }, {
 		name: 'chrome',
@@ -393,6 +394,7 @@ var gmail_ail_reply_mail_windows8 = {
 			[Date.UTC(2017, 9, 30), 133.33],
 			[Date.UTC(2017, 9, 30), 133.33],
 			[Date.UTC(2017, 9, 30), 133.33],
+			[Date.UTC(2017, 9, 30), 144.44],
 			[Date.UTC(2017, 9, 30), 144.44],
 			[Date.UTC(2017, 9, 30), 144.44],
 			[Date.UTC(2017, 9, 30), 144.44],
