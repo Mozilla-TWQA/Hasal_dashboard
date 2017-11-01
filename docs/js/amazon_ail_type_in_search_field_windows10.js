dcfa84364f36aa2a686528bd4d769679027cab95
MarkYan <mark11yan@gmail.com>
2017-11-01 08:36:38 +0800

    auto deploy on 08:36:38

--- docs/js/amazon_ail_type_in_search_field_windows10.js
+++ docs/js/amazon_ail_type_in_search_field_windows10.js
@@ -202,6 +202,7 @@ var amazon_ail_type_in_search_field_windows10 = {
 			[Date.UTC(2017, 9, 30), 22.22],
 			[Date.UTC(2017, 9, 30), 22.22],
 			[Date.UTC(2017, 9, 30), 22.22],
+			[Date.UTC(2017, 9, 30), 22.22],
 			[Date.UTC(2017, 9, 30), 33.33],
 			[Date.UTC(2017, 9, 30), 5.56],
 			[Date.UTC(2017, 9, 30), 5.56],
--- docs/js/facebook_ail_scroll_home_1_txt_windows8.js
+++ docs/js/facebook_ail_scroll_home_1_txt_windows8.js
@@ -220,6 +220,7 @@ var facebook_ail_scroll_home_1_txt_windows8 = {
 			[Date.UTC(2017, 9, 30), 55.56],
 			[Date.UTC(2017, 9, 30), 55.56],
 			[Date.UTC(2017, 9, 30), 55.56],
+			[Date.UTC(2017, 9, 30), 55.56],
 			[Date.UTC(2017, 9, 30), 66.67],
 			[Date.UTC(2017, 9, 30), 66.67],
 		]    }, {
@@ -380,6 +381,7 @@ var facebook_ail_scroll_home_1_txt_windows8 = {
 			[Date.UTC(2017, 9, 30), 11.11],
 			[Date.UTC(2017, 9, 30), 11.11],
 			[Date.UTC(2017, 9, 30), 11.11],
+			[Date.UTC(2017, 9, 30), 22.22],
 			[Date.UTC(2017, 9, 30), 22.22],
 			[Date.UTC(2017, 9, 30), 22.22],
 			[Date.UTC(2017, 9, 30), 22.22],
