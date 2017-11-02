664cd5dae58dbb6c6f94d9f99bb589508bcc1eb1
MarkYan <mark11yan@gmail.com>
2017-11-02 13:33:56 +0800

    auto deploy on 13:33:56

--- docs/js/facebook_ail_click_open_chat_tab_windows10.js
+++ docs/js/facebook_ail_click_open_chat_tab_windows10.js
@@ -389,5 +389,6 @@ var facebook_ail_click_open_chat_tab_windows10 = {
 			[Date.UTC(2017, 10, 1), 233.33],
 			[Date.UTC(2017, 10, 1), 300.0],
 			[Date.UTC(2017, 10, 1), 388.89],
+			[Date.UTC(2017, 10, 1), 422.22],
 		]    }]
 }
--- docs/js/facebook_ail_type_composerbox_1_txt_windows10.js
+++ docs/js/facebook_ail_type_composerbox_1_txt_windows10.js
@@ -188,6 +188,7 @@ var facebook_ail_type_composerbox_1_txt_windows10 = {
 			[Date.UTC(2017, 10, 1), 66.67],
 			[Date.UTC(2017, 10, 1), 22.22],
 			[Date.UTC(2017, 10, 1), 33.33],
+			[Date.UTC(2017, 10, 1), 44.44],
 			[Date.UTC(2017, 10, 1), 55.56],
 		]    }, {
 		name: 'chrome',
@@ -356,6 +357,7 @@ var facebook_ail_type_composerbox_1_txt_windows10 = {
 			[Date.UTC(2017, 10, 1), 5.56],
 			[Date.UTC(2017, 10, 1), 5.56],
 			[Date.UTC(2017, 10, 1), 11.11],
+			[Date.UTC(2017, 10, 1), 5.56],
 			[Date.UTC(2017, 10, 1), 5.56],
 			[Date.UTC(2017, 10, 1), 5.56],
 		]    }]
