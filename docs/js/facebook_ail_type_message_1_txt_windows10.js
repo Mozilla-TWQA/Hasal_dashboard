9dcd7c470a3e70942a4117858edb90b74433dc8b
MarkYan <mark11yan@gmail.com>
2017-11-04 00:39:21 +0800

    auto deploy on 00:39:21

--- docs/js/facebook_ail_type_message_1_txt_windows10.js
+++ docs/js/facebook_ail_type_message_1_txt_windows10.js
@@ -355,6 +355,7 @@ var facebook_ail_type_message_1_txt_windows10 = {
 			[Date.UTC(2017, 10, 2), 5.56],
 			[Date.UTC(2017, 10, 2), 5.56],
 			[Date.UTC(2017, 10, 2), 5.56],
+			[Date.UTC(2017, 10, 3), 11.11],
 			[Date.UTC(2017, 10, 3), 5.56],
 			[Date.UTC(2017, 10, 3), 5.56],
 			[Date.UTC(2017, 10, 3), 5.56],
--- docs/js/gsearch_ail_type_searchbox_windows10.js
+++ docs/js/gsearch_ail_type_searchbox_windows10.js
@@ -354,5 +354,6 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 10, 3), 5.56],
 			[Date.UTC(2017, 10, 3), 5.56],
 			[Date.UTC(2017, 10, 3), 5.56],
+			[Date.UTC(2017, 10, 3), 5.56],
 		]    }]
 }
