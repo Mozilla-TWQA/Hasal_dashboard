f8c994660d192c3a3728f260056b26dbc64b2c5d
MarkYan <mark11yan@gmail.com>
2017-11-04 00:13:44 +0800

    auto deploy on 00:13:44

--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
@@ -195,8 +195,10 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 10, 2), 233.33],
 			[Date.UTC(2017, 10, 2), 277.78],
 			[Date.UTC(2017, 10, 3), 200.0],
+			[Date.UTC(2017, 10, 3), 200.0],
 			[Date.UTC(2017, 10, 3), 244.44],
 			[Date.UTC(2017, 10, 3), 288.89],
+			[Date.UTC(2017, 10, 3), 300.0],
 			[Date.UTC(2017, 10, 3), 333.33],
 		]    }, {
 		name: 'chrome',
--- docs/js/gsearch_ail_type_searchbox_windows10.js
+++ docs/js/gsearch_ail_type_searchbox_windows10.js
@@ -197,6 +197,7 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 10, 2), 11.11],
 			[Date.UTC(2017, 10, 2), 5.56],
 			[Date.UTC(2017, 10, 3), 22.22],
+			[Date.UTC(2017, 10, 3), 44.44],
 			[Date.UTC(2017, 10, 3), 5.56],
 		]    }, {
 		name: 'chrome',
@@ -346,6 +347,7 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 10, 2), 5.56],
 			[Date.UTC(2017, 10, 2), 5.56],
 			[Date.UTC(2017, 10, 3), 11.11],
+			[Date.UTC(2017, 10, 3), 5.56],
 			[Date.UTC(2017, 10, 3), 5.56],
 			[Date.UTC(2017, 10, 3), 5.56],
 		]    }]
