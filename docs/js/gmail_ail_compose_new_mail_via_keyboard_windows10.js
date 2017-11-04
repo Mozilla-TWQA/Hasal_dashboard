32a1a235b9c030947d4d832092bc044eda53e3cb
MarkYan <mark11yan@gmail.com>
2017-11-05 00:59:04 +0800

    auto deploy on 00:59:04

--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
@@ -201,6 +201,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 10, 3), 311.11],
 			[Date.UTC(2017, 10, 3), 333.33],
 			[Date.UTC(2017, 10, 4), 200.0],
+			[Date.UTC(2017, 10, 4), 200.0],
 			[Date.UTC(2017, 10, 4), 233.33],
 			[Date.UTC(2017, 10, 4), 233.33],
 			[Date.UTC(2017, 10, 4), 255.56],
@@ -373,9 +374,11 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 10, 3), 166.67],
 			[Date.UTC(2017, 10, 4), 111.11],
 			[Date.UTC(2017, 10, 4), 122.22],
+			[Date.UTC(2017, 10, 4), 122.22],
 			[Date.UTC(2017, 10, 4), 133.33],
 			[Date.UTC(2017, 10, 4), 133.33],
 			[Date.UTC(2017, 10, 4), 133.33],
 			[Date.UTC(2017, 10, 4), 155.56],
+			[Date.UTC(2017, 10, 4), 188.89],
 		]    }]
 }
--- docs/js/gmail_ail_reply_mail_windows10.js
+++ docs/js/gmail_ail_reply_mail_windows10.js
@@ -213,6 +213,7 @@ var gmail_ail_reply_mail_windows10 = {
 			[Date.UTC(2017, 10, 4), 288.89],
 			[Date.UTC(2017, 10, 4), 300.0],
 			[Date.UTC(2017, 10, 4), 300.0],
+			[Date.UTC(2017, 10, 4), 311.11],
 		]    }, {
 		name: 'chrome',
 		data: [
