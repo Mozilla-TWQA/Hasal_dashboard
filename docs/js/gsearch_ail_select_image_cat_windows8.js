ed9502ec17da5358992136128aa0a85c8ef15f6d
MarkYan <mark11yan@gmail.com>
2017-11-07 13:23:52 +0800

    auto deploy on 13:23:52

--- docs/js/gsearch_ail_select_image_cat_windows8.js
+++ docs/js/gsearch_ail_select_image_cat_windows8.js
@@ -743,6 +743,7 @@ var gsearch_ail_select_image_cat_windows8 = {
 			[Date.UTC(2017, 10, 5), 155.56],
 			[Date.UTC(2017, 10, 5), 155.56],
 			[Date.UTC(2017, 10, 5), 155.56],
+			[Date.UTC(2017, 10, 5), 155.56],
 			[Date.UTC(2017, 10, 5), 166.67],
 			[Date.UTC(2017, 10, 5), 166.67],
 			[Date.UTC(2017, 10, 5), 166.67],
--- docs/js/youtube_ail_type_in_search_field_windows8.js
+++ docs/js/youtube_ail_type_in_search_field_windows8.js
@@ -417,6 +417,7 @@ var youtube_ail_type_in_search_field_windows8 = {
 			[Date.UTC(2017, 10, 5), 33.33],
 			[Date.UTC(2017, 10, 5), 33.33],
 			[Date.UTC(2017, 10, 5), 33.33],
+			[Date.UTC(2017, 10, 5), 33.33],
 			[Date.UTC(2017, 10, 5), 44.44],
 			[Date.UTC(2017, 10, 5), 44.44],
 			[Date.UTC(2017, 10, 5), 88.89],
