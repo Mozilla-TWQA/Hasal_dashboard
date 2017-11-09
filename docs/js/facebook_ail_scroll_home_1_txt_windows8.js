e00f3c3fe62e777a0de2882c59f1a83938666b2a
MarkYan <mark11yan@gmail.com>
2017-11-10 06:35:06 +0800

    auto deploy on 06:35:06

--- docs/js/facebook_ail_scroll_home_1_txt_windows8.js
+++ docs/js/facebook_ail_scroll_home_1_txt_windows8.js
@@ -856,6 +856,7 @@ var facebook_ail_scroll_home_1_txt_windows8 = {
 			[Date.UTC(2017, 10, 8), 44.44],
 			[Date.UTC(2017, 10, 8), 44.44],
 			[Date.UTC(2017, 10, 8), 44.44],
+			[Date.UTC(2017, 10, 8), 44.44],
 			[Date.UTC(2017, 10, 8), 55.56],
 			[Date.UTC(2017, 10, 8), 66.67],
 			[Date.UTC(2017, 10, 8), 66.67],
--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
@@ -423,5 +423,6 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 10, 8), 155.56],
 			[Date.UTC(2017, 10, 8), 155.56],
 			[Date.UTC(2017, 10, 8), 266.67],
+			[Date.UTC(2017, 10, 8), 544.44],
 		]    }]
 }
