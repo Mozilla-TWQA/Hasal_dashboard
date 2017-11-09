ee9f79dedc477af409af9d4c70c002e06ce47dba
MarkYan <mark11yan@gmail.com>
2017-11-10 00:34:11 +0800

    auto deploy on 00:34:11

--- docs/js/facebook_ail_click_photo_viewer_right_arrow_windows8.js
+++ docs/js/facebook_ail_click_photo_viewer_right_arrow_windows8.js
@@ -782,6 +782,7 @@ var facebook_ail_click_photo_viewer_right_arrow_windows8 = {
 			[Date.UTC(2017, 10, 8), 55.56],
 			[Date.UTC(2017, 10, 8), 55.56],
 			[Date.UTC(2017, 10, 8), 55.56],
+			[Date.UTC(2017, 10, 8), 55.56],
 			[Date.UTC(2017, 10, 8), 66.67],
 			[Date.UTC(2017, 10, 8), 66.67],
 			[Date.UTC(2017, 10, 8), 66.67],
--- docs/js/facebook_ail_type_message_1_txt_windows8.js
+++ docs/js/facebook_ail_type_message_1_txt_windows8.js
@@ -371,6 +371,7 @@ var facebook_ail_type_message_1_txt_windows8 = {
 			[Date.UTC(2017, 10, 7), 33.33],
 			[Date.UTC(2017, 10, 7), 44.44],
 			[Date.UTC(2017, 10, 7), 66.67],
+			[Date.UTC(2017, 10, 8), 22.22],
 			[Date.UTC(2017, 10, 8), 33.33],
 			[Date.UTC(2017, 10, 8), 33.33],
 			[Date.UTC(2017, 10, 8), 33.33],
@@ -380,6 +381,7 @@ var facebook_ail_type_message_1_txt_windows8 = {
 			[Date.UTC(2017, 10, 8), 55.56],
 			[Date.UTC(2017, 10, 8), 55.56],
 			[Date.UTC(2017, 10, 8), 55.56],
+			[Date.UTC(2017, 10, 8), 77.78],
 		]    }, {
 		name: 'chrome',
 		data: [
