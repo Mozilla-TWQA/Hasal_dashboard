4ce9ce39dbe237be6fb055f3b78bef5f00158c1b
MarkYan <mark11yan@gmail.com>
2017-11-10 00:21:16 +0800

    auto deploy on 00:21:16

--- docs/js/gdoc_ail_pagedown_10_text_windows10.js
+++ docs/js/gdoc_ail_pagedown_10_text_windows10.js
@@ -442,5 +442,6 @@ var gdoc_ail_pagedown_10_text_windows10 = {
 			[Date.UTC(2017, 10, 8), 33.33],
 			[Date.UTC(2017, 10, 8), 44.44],
 			[Date.UTC(2017, 10, 8), 55.56],
+			[Date.UTC(2017, 10, 8), 66.67],
 		]    }]
 }
