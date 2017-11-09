0b38c2a55300845d89943815b83782c22668acb0
MarkYan <mark11yan@gmail.com>
2017-11-10 00:53:29 +0800

    auto deploy on 00:53:29

--- docs/js/gsearch_ail_type_searchbox_windows10.js
+++ docs/js/gsearch_ail_type_searchbox_windows10.js
@@ -248,6 +248,7 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 10, 8), 5.56],
 			[Date.UTC(2017, 10, 8), 5.56],
 			[Date.UTC(2017, 10, 8), 5.56],
+			[Date.UTC(2017, 10, 8), 5.56],
 			[Date.UTC(2017, 10, 8), 66.67],
 		]    }, {
 		name: 'chrome',
