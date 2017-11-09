da5d55cfa80de63a7be2c82e2f3b1668fe17387e
MarkYan <mark11yan@gmail.com>
2017-11-10 07:13:31 +0800

    auto deploy on 07:13:31

--- docs/js/facebook_ail_type_composerbox_1_txt_windows8.js
+++ docs/js/facebook_ail_type_composerbox_1_txt_windows8.js
@@ -408,6 +408,7 @@ var facebook_ail_type_composerbox_1_txt_windows8 = {
 			[Date.UTC(2017, 10, 8), 77.78],
 			[Date.UTC(2017, 10, 8), 77.78],
 			[Date.UTC(2017, 10, 8), 77.78],
+			[Date.UTC(2017, 10, 8), 77.78],
 		]    }, {
 		name: 'chrome',
 		data: [
