9d28813a7193755c05c38b732aa07524976f1c54
MarkYan <mark11yan@gmail.com>
2017-11-13 00:34:43 +0800

    auto deploy on 00:34:43

--- docs/js/facebook_ail_click_open_chat_tab_emoji_windows8.js
+++ docs/js/facebook_ail_click_open_chat_tab_emoji_windows8.js
@@ -472,6 +472,7 @@ var facebook_ail_click_open_chat_tab_emoji_windows8 = {
 			[Date.UTC(2017, 10, 11), 66.67],
 			[Date.UTC(2017, 10, 11), 77.78],
 			[Date.UTC(2017, 10, 11), 77.78],
+			[Date.UTC(2017, 10, 11), 77.78],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -935,6 +936,8 @@ var facebook_ail_click_open_chat_tab_emoji_windows8 = {
 			[Date.UTC(2017, 10, 11), 55.56],
 			[Date.UTC(2017, 10, 11), 66.67],
 			[Date.UTC(2017, 10, 11), 66.67],
+			[Date.UTC(2017, 10, 11), 44.44],
+			[Date.UTC(2017, 10, 11), 44.44],
 			[Date.UTC(2017, 10, 11), 44.44],
 			[Date.UTC(2017, 10, 11), 44.44],
 			[Date.UTC(2017, 10, 11), 55.56],
--- docs/js/gsearch_ail_type_searchbox_windows10.js
+++ docs/js/gsearch_ail_type_searchbox_windows10.js
@@ -258,6 +258,7 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 10, 11), 66.67],
 			[Date.UTC(2017, 10, 11), 88.89],
 			[Date.UTC(2017, 10, 11), 11.11],
+			[Date.UTC(2017, 10, 11), 5.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -445,5 +446,6 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 10, 11), 66.67],
 			[Date.UTC(2017, 10, 11), 77.78],
 			[Date.UTC(2017, 10, 11), 33.33],
+			[Date.UTC(2017, 10, 11), 5.56],
 		]    }]
 }
