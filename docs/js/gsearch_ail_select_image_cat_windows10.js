8ec7f2f4b55a633724b4a78c25e2c747aee4a900
MarkYan <mark11yan@gmail.com>
2017-11-13 09:19:21 +0800

    auto deploy on 09:19:21

--- docs/js/gsearch_ail_select_image_cat_windows10.js
+++ docs/js/gsearch_ail_select_image_cat_windows10.js
@@ -446,6 +446,7 @@ var gsearch_ail_select_image_cat_windows10 = {
 			[Date.UTC(2017, 10, 11), 166.67],
 			[Date.UTC(2017, 10, 11), 166.67],
 			[Date.UTC(2017, 10, 11), 166.67],
+			[Date.UTC(2017, 10, 11), 177.78],
 			[Date.UTC(2017, 10, 11), 211.11],
 			[Date.UTC(2017, 10, 11), 222.22],
 			[Date.UTC(2017, 10, 11), 288.89],
