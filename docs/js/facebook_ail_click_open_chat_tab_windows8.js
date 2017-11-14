600e749bec78627d14b9749181f77eb132f5f336
MarkYan <mark11yan@gmail.com>
2017-11-15 02:06:22 +0800

    auto deploy on 02:06:22

--- docs/js/facebook_ail_click_open_chat_tab_windows8.js
+++ docs/js/facebook_ail_click_open_chat_tab_windows8.js
@@ -358,6 +358,7 @@ var facebook_ail_click_open_chat_tab_windows8 = {
 			[Date.UTC(2017, 10, 14), 144.44],
 			[Date.UTC(2017, 10, 14), 155.56],
 			[Date.UTC(2017, 10, 14), 155.56],
+			[Date.UTC(2017, 10, 14), 255.56],
 			[Date.UTC(2017, 10, 14), 311.11],
 			[Date.UTC(2017, 10, 14), 311.11],
 			[Date.UTC(2017, 10, 14), 322.22],
--- docs/js/youtube_ail_type_in_search_field_windows8.js
+++ docs/js/youtube_ail_type_in_search_field_windows8.js
@@ -804,6 +804,8 @@ var youtube_ail_type_in_search_field_windows8 = {
 			[Date.UTC(2017, 10, 13), 22.22],
 			[Date.UTC(2017, 10, 13), 22.22],
 			[Date.UTC(2017, 10, 13), 22.22],
+			[Date.UTC(2017, 10, 14), 11.11],
+			[Date.UTC(2017, 10, 14), 22.22],
 			[Date.UTC(2017, 10, 14), 22.22],
 			[Date.UTC(2017, 10, 14), 22.22],
 			[Date.UTC(2017, 10, 14), 33.33],
