fdd0ac690bc1ca510d926ab6f3fe00eb1f8acaca
MarkYan <mark11yan@gmail.com>
2017-11-14 10:17:38 +0800

    auto deploy on 10:17:38

--- docs/js/gmail_ail_reply_mail_windows8.js
+++ docs/js/gmail_ail_reply_mail_windows8.js
@@ -391,6 +391,8 @@ var gmail_ail_reply_mail_windows8 = {
 			[Date.UTC(2017, 10, 13), 266.67],
 			[Date.UTC(2017, 10, 13), 266.67],
 			[Date.UTC(2017, 10, 13), 233.33],
+			[Date.UTC(2017, 10, 13), 244.44],
+			[Date.UTC(2017, 10, 13), 255.56],
 			[Date.UTC(2017, 10, 13), 266.67],
 		]    }, {
 		name: 'chrome',
@@ -745,5 +747,7 @@ var gmail_ail_reply_mail_windows8 = {
 			[Date.UTC(2017, 10, 13), 166.67],
 			[Date.UTC(2017, 10, 13), 133.33],
 			[Date.UTC(2017, 10, 13), 144.44],
+			[Date.UTC(2017, 10, 13), 166.67],
+			[Date.UTC(2017, 10, 13), 166.67],
 		]    }]
 }
--- docs/js/gsearch_ail_select_image_cat_windows8.js
+++ docs/js/gsearch_ail_select_image_cat_windows8.js
@@ -406,6 +406,7 @@ var gsearch_ail_select_image_cat_windows8 = {
 			[Date.UTC(2017, 10, 13), 88.89],
 			[Date.UTC(2017, 10, 13), 77.78],
 			[Date.UTC(2017, 10, 13), 88.89],
+			[Date.UTC(2017, 10, 13), 88.89],
 		]    }, {
 		name: 'chrome',
 		data: [
