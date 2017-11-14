9aa6856bda5271f97cf85c32f92e143bc840b3f5
MarkYan <mark11yan@gmail.com>
2017-11-15 06:30:58 +0800

    auto deploy on 06:30:58

--- docs/js/facebook_ail_type_comment_1_txt_windows10.js
+++ docs/js/facebook_ail_type_comment_1_txt_windows10.js
@@ -399,6 +399,7 @@ var facebook_ail_type_comment_1_txt_windows10 = {
 			[Date.UTC(2017, 10, 14), 11.11],
 			[Date.UTC(2017, 10, 14), 22.22],
 			[Date.UTC(2017, 10, 14), 5.56],
+			[Date.UTC(2017, 10, 14), 5.56],
 			[Date.UTC(2017, 10, 14), 500.0],
 		]    }]
 }
--- docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
+++ docs/js/gmail_ail_compose_new_mail_via_keyboard_windows10.js
@@ -241,6 +241,7 @@ var gmail_ail_compose_new_mail_via_keyboard_windows10 = {
 			[Date.UTC(2017, 10, 14), 255.56],
 			[Date.UTC(2017, 10, 14), 266.67],
 			[Date.UTC(2017, 10, 14), 266.67],
+			[Date.UTC(2017, 10, 14), 288.89],
 		]    }, {
 		name: 'chrome',
 		data: [
