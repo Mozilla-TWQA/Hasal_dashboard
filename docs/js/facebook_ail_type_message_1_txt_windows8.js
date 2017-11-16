7130602b49a4c13142dfc41bb7374084c07edbcd
MarkYan <mark11yan@gmail.com>
2017-11-17 01:40:59 +0800

    auto deploy on 01:40:59

--- docs/js/facebook_ail_type_message_1_txt_windows8.js
+++ docs/js/facebook_ail_type_message_1_txt_windows8.js
@@ -613,5 +613,6 @@ var facebook_ail_type_message_1_txt_windows8 = {
 			[Date.UTC(2017, 10, 15), 33.33],
 			[Date.UTC(2017, 10, 15), 33.33],
 			[Date.UTC(2017, 10, 15), 33.33],
+			[Date.UTC(2017, 10, 15), 33.33],
 		]    }]
 }
--- docs/js/youtube_ail_type_in_search_field_windows8.js
+++ docs/js/youtube_ail_type_in_search_field_windows8.js
@@ -773,6 +773,7 @@ var youtube_ail_type_in_search_field_windows8 = {
 			[Date.UTC(2017, 10, 15), 11.11],
 			[Date.UTC(2017, 10, 15), 11.11],
 			[Date.UTC(2017, 10, 15), 11.11],
+			[Date.UTC(2017, 10, 15), 11.11],
 			[Date.UTC(2017, 10, 15), 22.22],
 			[Date.UTC(2017, 10, 15), 22.22],
 			[Date.UTC(2017, 10, 15), 22.22],
