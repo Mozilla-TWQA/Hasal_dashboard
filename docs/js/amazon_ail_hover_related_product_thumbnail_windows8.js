0d8c0288728ab91d28cd755635bfb640e191c88f
MarkYan <mark11yan@gmail.com>
2017-11-16 09:40:48 +0800

    auto deploy on 09:40:48

--- docs/js/amazon_ail_hover_related_product_thumbnail_windows8.js
+++ docs/js/amazon_ail_hover_related_product_thumbnail_windows8.js
@@ -354,6 +354,10 @@ var amazon_ail_hover_related_product_thumbnail_windows8 = {
 			[Date.UTC(2017, 10, 15), 500.0],
 			[Date.UTC(2017, 10, 15), 511.11],
 			[Date.UTC(2017, 10, 15), 533.33],
+			[Date.UTC(2017, 10, 15), 422.22],
+			[Date.UTC(2017, 10, 15), 433.33],
+			[Date.UTC(2017, 10, 15), 466.67],
+			[Date.UTC(2017, 10, 15), 477.78],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -685,6 +689,7 @@ var amazon_ail_hover_related_product_thumbnail_windows8 = {
 			[Date.UTC(2017, 10, 15), 555.56],
 			[Date.UTC(2017, 10, 15), 433.33],
 			[Date.UTC(2017, 10, 15), 477.78],
+			[Date.UTC(2017, 10, 15), 488.89],
 			[Date.UTC(2017, 10, 15), 533.33],
 		]    }]
 }
--- docs/js/gmail_ail_open_mail_windows10.js
+++ docs/js/gmail_ail_open_mail_windows10.js
@@ -215,6 +215,8 @@ var gmail_ail_open_mail_windows10 = {
 			[Date.UTC(2017, 10, 15), 266.67],
 			[Date.UTC(2017, 10, 15), 277.78],
 			[Date.UTC(2017, 10, 15), 355.56],
+			[Date.UTC(2017, 10, 15), 233.33],
+			[Date.UTC(2017, 10, 15), 288.89],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -409,5 +411,7 @@ var gmail_ail_open_mail_windows10 = {
 			[Date.UTC(2017, 10, 15), 288.89],
 			[Date.UTC(2017, 10, 15), 300.0],
 			[Date.UTC(2017, 10, 15), 244.44],
+			[Date.UTC(2017, 10, 15), 277.78],
+			[Date.UTC(2017, 10, 15), 322.22],
 		]    }]
 }
