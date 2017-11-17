bb7365d55768175c5a14a6f1c97a2f09d05baee9
MarkYan <mark11yan@gmail.com>
2017-11-18 01:04:34 +0800

    auto deploy on 01:04:34

--- docs/js/amazon_ail_hover_related_product_thumbnail_windows10.js
+++ docs/js/amazon_ail_hover_related_product_thumbnail_windows10.js
@@ -250,6 +250,7 @@ var amazon_ail_hover_related_product_thumbnail_windows10 = {
 			[Date.UTC(2017, 10, 17), 466.67],
 			[Date.UTC(2017, 10, 17), 488.89],
 			[Date.UTC(2017, 10, 17), 488.89],
+			[Date.UTC(2017, 10, 17), 500.0],
 		]    }, {
 		name: 'chrome',
 		data: [
--- docs/js/gsearch_ail_type_searchbox_windows10.js
+++ docs/js/gsearch_ail_type_searchbox_windows10.js
@@ -272,6 +272,7 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 10, 17), 11.11],
 			[Date.UTC(2017, 10, 17), 22.22],
 			[Date.UTC(2017, 10, 17), 5.56],
+			[Date.UTC(2017, 10, 17), 5.56],
 			[Date.UTC(2017, 10, 17), 55.56],
 		]    }, {
 		name: 'chrome',
@@ -465,6 +466,7 @@ var gsearch_ail_type_searchbox_windows10 = {
 			[Date.UTC(2017, 10, 16), 5.56],
 			[Date.UTC(2017, 10, 16), 5.56],
 			[Date.UTC(2017, 10, 16), 5.56],
+			[Date.UTC(2017, 10, 17), 5.56],
 			[Date.UTC(2017, 10, 17), 5.56],
 			[Date.UTC(2017, 10, 17), 5.56],
 			[Date.UTC(2017, 10, 17), 5.56],
