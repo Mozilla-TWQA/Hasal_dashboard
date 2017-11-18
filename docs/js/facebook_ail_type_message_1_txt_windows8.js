736803047d5f0eece56366cbab018e66d38f61fd
MarkYan <mark11yan@gmail.com>
2017-11-19 00:35:14 +0800

    auto deploy on 00:35:14

--- docs/js/facebook_ail_type_message_1_txt_windows8.js
+++ docs/js/facebook_ail_type_message_1_txt_windows8.js
@@ -633,6 +633,7 @@ var facebook_ail_type_message_1_txt_windows8 = {
 			[Date.UTC(2017, 10, 17), 33.33],
 			[Date.UTC(2017, 10, 17), 33.33],
 			[Date.UTC(2017, 10, 18), 11.11],
+			[Date.UTC(2017, 10, 18), 11.11],
 			[Date.UTC(2017, 10, 18), 33.33],
 			[Date.UTC(2017, 10, 18), 33.33],
 		]    }]
--- docs/js/gsearch_ail_select_image_cat_windows8.js
+++ docs/js/gsearch_ail_select_image_cat_windows8.js
@@ -771,6 +771,7 @@ var gsearch_ail_select_image_cat_windows8 = {
 			[Date.UTC(2017, 10, 17), 55.56],
 			[Date.UTC(2017, 10, 17), 88.89],
 			[Date.UTC(2017, 10, 18), 111.11],
+			[Date.UTC(2017, 10, 18), 133.33],
 			[Date.UTC(2017, 10, 18), 144.44],
 		]    }]
 }
