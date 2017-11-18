a0a0906af9a4b12c4b8e28f5b28899cd2a8a5889
MarkYan <mark11yan@gmail.com>
2017-11-18 22:52:50 +0800

    auto deploy on 22:52:50

--- docs/js/facebook_ail_type_message_1_txt_windows10.js
+++ docs/js/facebook_ail_type_message_1_txt_windows10.js
@@ -212,6 +212,7 @@ var facebook_ail_type_message_1_txt_windows10 = {
 			[Date.UTC(2017, 10, 18), 44.44],
 			[Date.UTC(2017, 10, 18), 44.44],
 			[Date.UTC(2017, 10, 18), 55.56],
+			[Date.UTC(2017, 10, 18), 77.78],
 		]    }, {
 		name: 'chrome',
 		data: [
--- docs/js/youtube_ail_type_in_search_field_windows8.js
+++ docs/js/youtube_ail_type_in_search_field_windows8.js
@@ -788,5 +788,6 @@ var youtube_ail_type_in_search_field_windows8 = {
 			[Date.UTC(2017, 10, 17), 11.11],
 			[Date.UTC(2017, 10, 17), 22.22],
 			[Date.UTC(2017, 10, 17), 33.33],
+			[Date.UTC(2017, 10, 18), 11.11],
 		]    }]
 }
