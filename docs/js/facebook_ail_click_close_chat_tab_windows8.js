5a2b59bc17e71a547aa5ff58d755e99c309a1650
MarkYan <mark11yan@gmail.com>
2017-11-19 02:49:42 +0800

    auto deploy on 02:49:42

--- docs/js/facebook_ail_click_close_chat_tab_windows8.js
+++ docs/js/facebook_ail_click_close_chat_tab_windows8.js
@@ -445,6 +445,7 @@ var facebook_ail_click_close_chat_tab_windows8 = {
 			[Date.UTC(2017, 10, 18), 66.67],
 			[Date.UTC(2017, 10, 18), 66.67],
 			[Date.UTC(2017, 10, 18), 77.78],
+			[Date.UTC(2017, 10, 18), 77.78],
 			[Date.UTC(2017, 10, 18), 88.89],
 		]    }, {
 		name: 'chrome',
@@ -823,6 +824,7 @@ var facebook_ail_click_close_chat_tab_windows8 = {
 			[Date.UTC(2017, 10, 17), 66.67],
 			[Date.UTC(2017, 10, 17), 77.78],
 			[Date.UTC(2017, 10, 17), 77.78],
+			[Date.UTC(2017, 10, 18), 55.56],
 			[Date.UTC(2017, 10, 18), 66.67],
 			[Date.UTC(2017, 10, 18), 66.67],
 			[Date.UTC(2017, 10, 18), 66.67],
--- docs/js/facebook_ail_click_photo_viewer_right_arrow_windows8.js
+++ docs/js/facebook_ail_click_photo_viewer_right_arrow_windows8.js
@@ -408,6 +408,7 @@ var facebook_ail_click_photo_viewer_right_arrow_windows8 = {
 			[Date.UTC(2017, 10, 18), 111.11],
 			[Date.UTC(2017, 10, 18), 122.22],
 			[Date.UTC(2017, 10, 18), 88.89],
+			[Date.UTC(2017, 10, 18), 88.89],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -791,6 +792,7 @@ var facebook_ail_click_photo_viewer_right_arrow_windows8 = {
 			[Date.UTC(2017, 10, 17), 77.78],
 			[Date.UTC(2017, 10, 17), 77.78],
 			[Date.UTC(2017, 10, 17), 77.78],
+			[Date.UTC(2017, 10, 18), 55.56],
 			[Date.UTC(2017, 10, 18), 55.56],
 			[Date.UTC(2017, 10, 18), 55.56],
 			[Date.UTC(2017, 10, 18), 55.56],
