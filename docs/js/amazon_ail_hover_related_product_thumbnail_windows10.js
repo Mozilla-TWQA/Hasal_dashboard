1955b10430c3fc7a027ac883dc4606896e50dbac
MarkYan <mark11yan@gmail.com>
2017-11-19 12:52:28 +0800

    auto deploy on 12:52:28

--- docs/js/amazon_ail_hover_related_product_thumbnail_windows10.js
+++ docs/js/amazon_ail_hover_related_product_thumbnail_windows10.js
@@ -243,6 +243,7 @@ var amazon_ail_hover_related_product_thumbnail_windows10 = {
 			[Date.UTC(2017, 10, 18), 477.78],
 			[Date.UTC(2017, 10, 18), 477.78],
 			[Date.UTC(2017, 10, 18), 488.89],
+			[Date.UTC(2017, 10, 18), 488.89],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -444,5 +445,6 @@ var amazon_ail_hover_related_product_thumbnail_windows10 = {
 			[Date.UTC(2017, 10, 18), 500.0],
 			[Date.UTC(2017, 10, 18), 500.0],
 			[Date.UTC(2017, 10, 18), 500.0],
+			[Date.UTC(2017, 10, 18), 511.11],
 		]    }]
 }
--- docs/js/youtube_ail_select_search_suggestion_windows10.js
+++ docs/js/youtube_ail_select_search_suggestion_windows10.js
@@ -216,6 +216,7 @@ var youtube_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 10, 18), 5.56],
 			[Date.UTC(2017, 10, 18), 5.56],
 			[Date.UTC(2017, 10, 18), 22.22],
+			[Date.UTC(2017, 10, 18), 22.22],
 			[Date.UTC(2017, 10, 18), 5.56],
 			[Date.UTC(2017, 10, 18), 5.56],
 		]    }, {
@@ -417,6 +418,7 @@ var youtube_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 10, 18), 5.56],
 			[Date.UTC(2017, 10, 18), 5.56],
 			[Date.UTC(2017, 10, 18), 11.11],
+			[Date.UTC(2017, 10, 18), 5.56],
 			[Date.UTC(2017, 10, 18), 5.56],
 			[Date.UTC(2017, 10, 18), 5.56],
 		]    }]
