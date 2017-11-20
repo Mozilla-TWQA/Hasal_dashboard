b74f1611e6b759765aaa7a2e0eacf7c5b6f1bf9d
MarkYan <mark11yan@gmail.com>
2017-11-20 11:19:38 +0800

    auto deploy on 11:19:38

--- docs/js/youtube_ail_select_search_suggestion_windows10.js
+++ docs/js/youtube_ail_select_search_suggestion_windows10.js
@@ -201,6 +201,8 @@ var youtube_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 10, 19), 33.33],
 			[Date.UTC(2017, 10, 19), 5.56],
 			[Date.UTC(2017, 10, 19), 22.22],
+			[Date.UTC(2017, 10, 19), 22.22],
+			[Date.UTC(2017, 10, 19), 5.56],
 		]    }, {
 		name: 'chrome',
 		data: [
@@ -372,6 +374,8 @@ var youtube_ail_select_search_suggestion_windows10 = {
 			[Date.UTC(2017, 10, 18), 5.56],
 			[Date.UTC(2017, 10, 18), 5.56],
 			[Date.UTC(2017, 10, 19), 22.22],
+			[Date.UTC(2017, 10, 19), 5.56],
+			[Date.UTC(2017, 10, 19), 5.56],
 			[Date.UTC(2017, 10, 19), 5.56],
 			[Date.UTC(2017, 10, 19), 5.56],
 			[Date.UTC(2017, 10, 19), 5.56],
